3215aca3057dbca2a832c2316c8c2c701ab2ff84
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2024-10-15 16:02:04 +0100

    add read hardware for zocalo test

--- src/mx_bluesky/common/device_setup_plans/read_hardware_for_setup.py
+++ src/mx_bluesky/common/device_setup_plans/read_hardware_for_setup.py
@@ -5,6 +5,7 @@ from mx_bluesky.parameters.constants import MxConstants
 
 
 def read_hardware_for_zocalo(detector: EigerDetector):
+    # Bluesky run must be open to use this plan
     yield from bps.create(name=MxConstants.DESCRIPTORS.ZOCALO_HW_READ)
     yield from bps.read(detector.odin.file_writer.id)  # type: ignore # See: https://github.com/bluesky/bluesky/issues/1809
     yield from bps.save()
