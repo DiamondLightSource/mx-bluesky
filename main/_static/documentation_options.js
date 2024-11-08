bc98c12d93d8dd13384ebdb698564a206a6b1198
dperl-dls <dperl-dls@users.noreply.github.com>
2024-11-08 10:03:16 +0000

    deploy: d3983c64924ddab9c07f889688d24302058982c0

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.0.1.dev59+g113401817',
+    VERSION: '1.0.1.dev60+gd3983c649',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
