d01f029db8595d5e663a84fbdbb5d73ea1823fcb
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2024-11-14 11:32:29 +0000

    Try to improve coverage again

--- tests/unit_tests/beamlines/i24/serial/setup_beamline/test_setup_beamline.py
+++ tests/unit_tests/beamlines/i24/serial/setup_beamline/test_setup_beamline.py
@@ -76,3 +76,20 @@ def test_eiger_triggered(_, fake_caget, fake_caput):
     setup_beamline.eiger("triggered", ["", "", "10", "0.1"])
     assert fake_caget.call_count == 4
     assert fake_caput.call_count == 32
+
+
+@pytest.mark.parametrize(
+    "action, expected_caputs, expected_sleeps",
+    [
+        ("Pin_hand_mount", 11, 0),
+        ("Pin_rt_hand_mount", 11, 0),
+        ("Pin_data_collection", 12, 2),
+        ("Pin_rt_data_collection", 13, 2),
+    ],
+)
+@patch("mx_bluesky.beamlines.i24.serial.setup_beamline.setup_beamline.caput")
+@patch("mx_bluesky.beamlines.i24.serial.setup_beamline.setup_beamline.sleep")
+def test_mode_change(fake_sleep, fake_caput, action, expected_caputs, expected_sleeps):
+    setup_beamline.modechange(action)
+    assert fake_caput.call_count == expected_caputs
+    assert fake_sleep.call_count == expected_sleeps
