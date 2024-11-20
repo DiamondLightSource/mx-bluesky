74763f665170101cfbc18546ed6e7d9f435b153c
co2e14 <christian.orr@diamond.ac.uk>
2024-11-20 15:43:39 +0000

    trying to get OAV to work

---
+++ src/mx_bluesky/beamlines/aithre_lasershaping/parameters/constants.py
@@ -0,0 +1,5 @@
+AV_CONFIG_FILES = {
+    "zoom_params_file": "/scratch/vwg86669/mx-bluesky/src/mx-bluesky/beamline/aithre_lasershaping/zoom_levels.xml",
+    "oav_config_json": "/scratch/vwg85559/dodal/src/dodal/devices/aithre_lasershaping/OAV.json",
+}
+OAV_CAM = "http://bl23i-ea-serv-01.diamond.ac.uk:8080/OAV.mjpg.mjpg"
