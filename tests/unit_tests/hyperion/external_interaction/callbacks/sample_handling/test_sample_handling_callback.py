164e14ccc2de337f26166c89764cfa144951633b
Robert Tuck <robert.tuck@diamond.ac.uk>
2024-11-22 13:24:50 +0000

    Enhance coverage of sample_handling_callback unit tests

--- tests/unit_tests/hyperion/external_interaction/callbacks/sample_handling/test_sample_handling_callback.py
+++ tests/unit_tests/hyperion/external_interaction/callbacks/sample_handling/test_sample_handling_callback.py
@@ -1,13 +1,18 @@
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 import pytest
 from bluesky.preprocessors import run_decorator
 from bluesky.run_engine import RunEngine
 
+from mx_bluesky.hyperion.exceptions import SampleException
+from mx_bluesky.hyperion.experiment_plans.flyscan_xray_centre_plan import (
+    CrystalNotFoundException,
+)
 from mx_bluesky.hyperion.external_interaction.callbacks.sample_handling.sample_handling_callback import (
     SampleHandlingCallback,
     sample_handling_callback_decorator,
 )
+from mx_bluesky.hyperion.external_interaction.ispyb.exp_eye_store import BLSampleStatus
 
 TEST_SAMPLE_ID = 123456
 
@@ -19,9 +24,9 @@ TEST_SAMPLE_ID = 123456
     }
 )
 @sample_handling_callback_decorator()
-def plan_with_general_exception():
+def plan_with_general_exception(exception_type: type):
     yield from []
-    raise AssertionError("Test failure")
+    raise exception_type("Test failure")
 
 
 @run_decorator(
@@ -35,17 +40,33 @@ def plan_with_normal_completion():
     yield from []
 
 
-def test_sample_handling_callback_intercepts_general_exception(RE: RunEngine):
+@pytest.mark.parametrize(
+    "exception_type, expected_sample_status",
+    [
+        [AssertionError, BLSampleStatus.ERROR_BEAMLINE],
+        [SampleException, BLSampleStatus.ERROR_SAMPLE],
+        [CrystalNotFoundException, BLSampleStatus.ERROR_SAMPLE],
+    ],
+)
+def test_sample_handling_callback_intercepts_general_exception(
+    RE: RunEngine, exception_type: type, expected_sample_status: BLSampleStatus
+):
     callback = SampleHandlingCallback()
     RE.subscribe(callback)
 
+    mock_expeye = MagicMock()
     with (
-        patch.object(callback, "_record_exception") as record_exception,
-        pytest.raises(AssertionError),
+        patch(
+            "mx_bluesky.hyperion.external_interaction.callbacks.sample_handling.sample_handling_callback"
+            ".ExpeyeSampleHandlingInteraction",
+            return_value=mock_expeye,
+        ),
+        pytest.raises(exception_type),
     ):
-        RE(plan_with_general_exception())
-
-    record_exception.assert_called_once_with("AssertionError")
+        RE(plan_with_general_exception(exception_type))
+    mock_expeye.update_sample_status.assert_called_once_with(
+        TEST_SAMPLE_ID, expected_sample_status
+    )
 
 
 def test_sample_handling_callback_closes_run_normally(RE: RunEngine):
