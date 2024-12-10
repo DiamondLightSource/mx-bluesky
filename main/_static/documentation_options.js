5ae3f867e07ac0637aa32404743565bf2bdf1b81
olliesilvester <olliesilvester@users.noreply.github.com>
2024-12-10 14:15:19 +0000

    deploy: 1f3cb6178f7dcaa9f0fbbc0e728e57b2dcfc1b3b

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.0.1.dev93+g5497217c3',
+    VERSION: '1.0.1.dev94+g1f3cb6178',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
