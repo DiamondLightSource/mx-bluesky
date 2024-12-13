2502750506cacc853afa9bd873e9a9f7e06936b1
shihab-dls <shihab-dls@users.noreply.github.com>
2024-12-13 14:11:40 +0000

    deploy: 8f6b23d5a5689912bc02cf9443d3a8af38a2771c

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.0.1.dev97+g0634f9336',
+    VERSION: '1.0.1.dev98+g8f6b23d5a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
