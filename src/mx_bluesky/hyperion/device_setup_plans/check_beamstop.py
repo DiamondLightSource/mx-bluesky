c3f328b04de85c422c499c0b4582c04e34ae3841
Robert Tuck <robert.tuck@diamond.ac.uk>
2024-12-20 10:53:31 +0000

    Pin dodal, fix import

--- src/mx_bluesky/hyperion/device_setup_plans/check_beamstop.py
+++ src/mx_bluesky/hyperion/device_setup_plans/check_beamstop.py
@@ -1,7 +1,7 @@
 import bluesky.plan_stubs as bps
 from dodal.devices.i03.beamstop import Beamstop, BeamstopPositions
 
-from mx_bluesky.hyperion.log import LOGGER
+from mx_bluesky.common.utils.log import LOGGER
 
 
 class BeamstopException(Exception):
