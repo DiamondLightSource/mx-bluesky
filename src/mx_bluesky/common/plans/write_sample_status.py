940cee4583aceca0e030901eacd97beef9dfefb2
Shihab Suliman <rye74444@ws557.diamond.ac.uk>
2025-01-10 11:07:17 +0000

    Expose plans to GDA

--- src/mx_bluesky/common/plans/write_sample_status.py
+++ src/mx_bluesky/common/plans/write_sample_status.py
@@ -1,7 +1,20 @@
 import bluesky.plan_stubs as bps
 import bluesky.preprocessors as bpp
 
+from mx_bluesky.common.parameters.components import (
+    MxBlueskyParameters,
+    WithSample,
+)
 from mx_bluesky.common.utils.exceptions import SampleException
+
+
+class WriteSampleStatus(MxBlueskyParameters, WithSample):
+    pass
+
+
+def create_devices() -> None:
+    """Create the necessary devices for the plan."""
+    return None
 
 
 def deposit_sample_error(exception_type, sample_id):
