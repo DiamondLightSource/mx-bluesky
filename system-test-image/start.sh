1f42b9407f05f362366af59d12d238f5c91816d7
Robert Tuck <robert.tuck@diamond.ac.uk>
2025-01-17 14:48:36 +0000

    Update entrypoint script to fetch a new token every time

--- system-test-image/start.sh
+++ system-test-image/start.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-./config.sh  --url https://github.com/DiamondLightSource/mx-bluesky \
-  --name "mx-bluesky-system-test" \
-  --token ${TOKEN} \
-  --unattended
+echo "Fetching token"
+REG_TOKEN=$(curl -X POST \
+  -H "Authorization: token ${TOKEN}" \
+  -H "Accept: application/vnd.github+json" \
+  https://api.github.com/repos/DiamondLightSource/mx-bluesky/actions/runners/registration-token | \
+  jq .token --raw-output)
+
+./config.sh  \
+  --unattended \
+  --url https://github.com/DiamondLightSource/mx-bluesky \
+  --token ${REG_TOKEN} \
+  --labels system-tests \
+  --name "mx-bluesky-system-test" && \
+  echo "Registered successfully"
 
 cleanup() {
   echo "Removing runner.."
