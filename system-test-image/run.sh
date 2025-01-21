923cd6f201a9c115375fdb2ca75b7fd38257024b
Robert Tuck <robert.tuck@diamond.ac.uk>
2025-01-21 15:19:33 +0000

    Add documentation on system test containerisation Helmcharts for deployment to k8s cluster Updates to modify secrets handling

--- system-test-image/run.sh
+++ system-test-image/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Run the system test image locally
+# Run the system test image locally in podman for debugging
 # to remove the runner
-# podman compose -f utility_scripts/docker/system-test-compose.yml down
+# podman compose -f $PROJECTDIR/utility_scripts/docker/system-test-compose.yml down
 PROJECTDIR=$(realpath $(dirname $0)/..)
 SECRETS_DIR=$HOME/.secrets podman compose -f $PROJECTDIR/utility_scripts/docker/system-test-compose.yml up -d
