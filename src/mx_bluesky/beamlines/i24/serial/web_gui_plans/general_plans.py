6130760156c7d3efb45d4c76637b21d2f5d1f434
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-01-21 11:33:36 +0000

    Beamline testing - to be tidied up

--- src/mx_bluesky/beamlines/i24/serial/web_gui_plans/general_plans.py
+++ src/mx_bluesky/beamlines/i24/serial/web_gui_plans/general_plans.py
@@ -1,4 +1,4 @@
-from collections.abc import Sequence
+# from collections.abc import Sequence
 from typing import Literal
 
 import bluesky.plan_stubs as bps
@@ -89,23 +89,24 @@ def gui_set_parameters(
     transmission: float,
     n_shots: int,
     chip_type: str,
-    pump_settings: Sequence,
-    use_mapping_lite: bool,
-    chipmap: Sequence = (),
-):
+    checker_pattern: bool,
+    # pump_settings: Sequence,
+    # use_mapping_lite: bool = False,
+    # chipmap: Sequence = (),
+) -> MsgGenerator:
     detector_stage = i24.detector_motion()
     det_type = yield from get_detector_type(detector_stage)
     chip_params = get_chip_format(ChipType[chip_type])
-    pump_repeat = (
-        PumpProbeSetting.NoPP
-        if not pump_settings[0]
-        else PumpProbeSetting[pump_settings[0]]
-    )
-    map_type = MappingType.Lite if use_mapping_lite else MappingType.NoMap
+    # pump_repeat = (
+    #     PumpProbeSetting.NoPP
+    #     if not pump_settings[0]
+    #     else PumpProbeSetting[pump_settings[0]]
+    # )
+    # map_type = MappingType.Lite if use_mapping_lite else MappingType.NoMap
 
-    if chip_params.chip_type is ChipType.Oxford:
-        if use_mapping_lite and len(chipmap) == 0:
-            raise ValueError("Mapping lite chosen but no chipmap")
+    # if chip_params.chip_type is ChipType.Oxford:
+    #     if use_mapping_lite and len(chipmap) == 0:
+    #         raise ValueError("Mapping lite chosen but no chipmap")
 
     params = {
         "visit": _read_visit_directory_from_file().as_posix(),  # noqa
@@ -117,16 +118,16 @@ def gui_set_parameters(
         "num_exposures": n_shots,
         "transmission": transmission,
         "chip": chip_params,
-        "map_type": map_type,
-        "chip_map": list(chipmap),
-        "pump_repeat": pump_repeat,
-        "laser_dwell_s": pump_settings[1]
-        if pump_repeat != PumpProbeSetting.NoPP
-        else 0.0,
-        "laser_delay_s": pump_settings[2]
-        if pump_repeat != PumpProbeSetting.NoPP
-        else 0.0,
-        "checker_pattern": pump_settings[3],
+        "map_type": MappingType.NoMap,  # map_type,
+        "chip_map": [],  # list(chipmap),
+        "pump_repeat": PumpProbeSetting.NoPP,  # pump_repeat,
+        "laser_dwell_s": 0.0,  # pump_settings[1]
+        # if pump_repeat != PumpProbeSetting.NoPP
+        # else 0.0,
+        "laser_delay_s": 0.0,  # pump_settings[2]
+        # if pump_repeat != PumpProbeSetting.NoPP
+        # else 0.0,
+        "checker_pattern": checker_pattern,  # pump_settings[3],
         "pre_pump_exposure_s": None,
     }
     # At some point it will use FixedTargetParameters to create the parameters model
