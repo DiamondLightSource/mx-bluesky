ec54a2174cbcdfe8c5809230b92e542ef2613ea0
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-01-24 11:04:55 +0000

    Some more test fixes  but save this time

--- tests/unit_tests/beamlines/i24/jungfrau_commissioning/test_dark_plans.py
+++ tests/unit_tests/beamlines/i24/jungfrau_commissioning/test_dark_plans.py
@@ -2,13 +2,11 @@ from unittest.mock import MagicMock, patch
 
 from bluesky.run_engine import RunEngine
 from dodal.devices.i24.jungfrau import GainMode, JungFrau1M
+from ophyd_async.testing import get_mock
 
 from mx_bluesky.beamlines.i24.jungfrau_commissioning.plans.gain_mode_darks_plans import (
     do_manual_acquisition,
     set_gain_mode,
-)
-from mx_bluesky.beamlines.i24.jungfrau_commissioning.utils.jf_commissioning_devices import (
-    JungfrauM1,
 )
 
 
@@ -23,11 +21,11 @@ async def test_set_gain_mode(
     jungfrau: JungFrau1M = fake_devices["jungfrau"]
 
     RE(set_gain_mode(jungfrau, GainMode.DYNAMIC))
-    assert jungfrau.gain_mode.get() == "dynamic"
-    RE(set_gain_mode(jungfrau, GainMode.forceswitchg1))
-    assert jungfrau.gain_mode.get() == "forceswitchg1"
-    RE(set_gain_mode(jungfrau, GainMode.forceswitchg2))
-    assert jungfrau.gain_mode.get() == "forceswitchg2"
+    assert await jungfrau.gain_mode.get_value() == "dynamic"
+    RE(set_gain_mode(jungfrau, GainMode.FORCESWITCHG1))
+    assert await jungfrau.gain_mode.get_value() == "forceswitchg1"
+    RE(set_gain_mode(jungfrau, GainMode.FORCESWITCHG2))
+    assert await jungfrau.gain_mode.get_value() == "forceswitchg2"
 
 
 @patch(
@@ -40,7 +38,8 @@ def test_do_dark_acq(
 ):
     # gonio: VGonio = fake_devices["gonio"]
     # zebra: Zebra = fake_devices["zebra"]
-    jungfrau: JungfrauM1 = fake_devices["jungfrau"]
+    jungfrau: JungFrau1M = fake_devices["jungfrau"]
 
     RE(do_manual_acquisition(jungfrau, 0.001, 0.001, 1000))
-    jungfrau.acquire_start.set.assert_called()  # type: ignore
+    acquire_mock = get_mock(jungfrau.acquire_start)
+    acquire_mock.set.assert_called()  # type: ignore
