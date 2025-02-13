f1429520f367a12fd5a729fcf16c050d7d9d31f3
Robert Tuck <robert.tuck@diamond.ac.uk>
2025-02-13 10:31:29 +0000

    Make type-checking happy

--- src/mx_bluesky/common/external_interaction/ispyb/ispyb_utils.py
+++ src/mx_bluesky/common/external_interaction/ispyb/ispyb_utils.py
@@ -8,8 +8,10 @@ from ispyb.connector.mysqlsp.main import ISPyBMySQLSPConnector as Connector
 from ispyb.sp.core import Core
 
 
-def get_ispyb_config():
-    return os.environ.get("ISPYB_CONFIG_PATH")
+def get_ispyb_config() -> str:
+    ispyb_config = os.environ.get("ISPYB_CONFIG_PATH")
+    assert ispyb_config, "ISPYB_CONFIG_PATH must be set"
+    return ispyb_config
 
 
 def get_session_id_from_visit(conn: Connector, visit: str):
