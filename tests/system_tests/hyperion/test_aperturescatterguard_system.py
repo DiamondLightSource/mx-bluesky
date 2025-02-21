09495680e015cf66e1d75525e6d43d2ad0ebcb0a
Dominic Oram <dominic.oram@diamond.ac.uk>
2025-02-21 16:59:41 +0000

    Remove aperture change callback

--- tests/system_tests/hyperion/test_aperturescatterguard_system.py
+++ tests/system_tests/hyperion/test_aperturescatterguard_system.py
@@ -10,10 +10,6 @@ from dodal.devices.aperturescatterguard import (
 )
 from ophyd_async.core import init_devices
 
-from mx_bluesky.hyperion.experiment_plans.change_aperture_then_move_plan import (
-    set_aperture_for_bbox_mm,
-)
-
 
 @pytest.fixture
 def ap_sg():
@@ -26,28 +22,3 @@ def ap_sg():
             tolerances=AperturePosition.tolerances_from_gda_params(params),
         )
     return ap_sg
-
-
-@pytest.mark.s03()
-@pytest.mark.parametrize(
-    "bbox, expected_aperture",
-    [
-        ([0.05, 0.05, 0.05], "LARGE_APERTURE"),
-        ([0.02, 0.02, 0.02], "MEDIUM_APERTURE"),
-    ],
-    ids=["large_aperture", "medium_aperture"],
-)
-def test_aperture_change_callback(
-    ap_sg: ApertureScatterguard, bbox: list[float], expected_aperture: str
-):
-    from bluesky.run_engine import RunEngine
-
-    from mx_bluesky.hyperion.external_interaction.callbacks.aperture_change_callback import (
-        ApertureChangeCallback,
-    )
-
-    cb = ApertureChangeCallback()
-    RE = RunEngine({})
-    RE.subscribe(cb)
-    RE(set_aperture_for_bbox_mm(ap_sg, bbox))
-    assert cb.last_selected_aperture == expected_aperture
