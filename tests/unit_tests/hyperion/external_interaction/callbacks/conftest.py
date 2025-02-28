ca6813c3ce9aa4630581963f62ddf13bf2954207
Robert Tuck <robert.tuck@diamond.ac.uk>
2025-02-28 13:07:10 +0000

    WIP

--- tests/unit_tests/hyperion/external_interaction/callbacks/conftest.py
+++ tests/unit_tests/hyperion/external_interaction/callbacks/conftest.py
@@ -1,10 +1,13 @@
+from pathlib import Path
+
 import pytest
 
 from mx_bluesky.hyperion.parameters.constants import CONST
 
 
 @pytest.fixture
-def test_rotation_start_outer_document(dummy_rotation_params):
+def test_rotation_start_outer_document(dummy_rotation_params, tmp_path: Path):
+    dummy_rotation_params.storage_directory = str(tmp_path)
     return {
         "uid": "d8bee3ee-f614-4e7a-a516-25d6b9e87ef3",
         "subplan_name": CONST.PLAN.ROTATION_OUTER,
