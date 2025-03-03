d9e290026e38a87111526b31abf8098bd9ceda4e
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2025-03-03 14:33:56 +0000

    Fix typing

--- src/mx_bluesky/common/parameters/write_sample_status.py
+++ src/mx_bluesky/common/parameters/write_sample_status.py
@@ -1,11 +1,14 @@
 from mx_bluesky.common.parameters.components import (
     MxBlueskyParameters,
     WithSample,
+    WithVisit,
 )
 
 
-class WriteSampleStatus(MxBlueskyParameters, WithSample):
+class WriteSampleStatus(MxBlueskyParameters, WithSample, WithVisit):
     exception_type: str
+    visit: str = ""
 
 
-class WriteSampleLoaded(MxBlueskyParameters, WithSample): ...
+class WriteSampleLoaded(MxBlueskyParameters, WithSample, WithVisit):
+    visit: str = ""
