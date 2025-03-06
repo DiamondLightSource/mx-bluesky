18c298b8245cc184657ca7a6adb6093471a4b5c0
Robert Tuck <robert.tuck@diamond.ac.uk>
2025-03-06 10:50:55 +0000

    Fix unit tests

--- tests/unit_tests/hyperion/external_interaction/callbacks/conftest.py
+++ tests/unit_tests/hyperion/external_interaction/callbacks/conftest.py
@@ -1,13 +1,10 @@
-from pathlib import Path
-
 import pytest
 
 from mx_bluesky.hyperion.parameters.constants import CONST
 
 
 @pytest.fixture
-def test_rotation_start_outer_document(dummy_rotation_params, tmp_path: Path):
-    dummy_rotation_params.storage_directory = str(tmp_path)
+def test_rotation_start_outer_document(dummy_rotation_params):
     return {
         "uid": "d8bee3ee-f614-4e7a-a516-25d6b9e87ef3",
         "subplan_name": CONST.PLAN.ROTATION_OUTER,
