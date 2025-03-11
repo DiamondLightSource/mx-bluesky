76afcb221514ade5c75316c0133e67b7de2f13e2
jacob720 <jacob720@users.noreply.github.com>
2025-03-11 11:05:19 +0000

    deploy: 13143d918f18b9bb2839a8adee185c4969ac4cc0

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.0.1.dev177+g047a740ac',
+    VERSION: '1.0.1.dev178+g13143d918',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
