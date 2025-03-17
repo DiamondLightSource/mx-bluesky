a4e7c58972da8422b163ab1dd1031ea704f46616
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2025-03-17 15:09:11 +0000

    Use common plan in hyperion plan

--- src/mx_bluesky/hyperion/experiment_plans/grid_detect_then_xray_centre_plan.py
+++ src/mx_bluesky/hyperion/experiment_plans/grid_detect_then_xray_centre_plan.py
@@ -1,16 +1,10 @@
 from __future__ import annotations
 
-from pathlib import Path
-
 import pydantic
-from blueapi.core import BlueskyContext
-from bluesky import plan_stubs as bps
-from bluesky import preprocessors as bpp
-from bluesky.preprocessors import subs_decorator
 from bluesky.utils import MsgGenerator
 from dodal.devices.aperturescatterguard import ApertureScatterguard
 from dodal.devices.attenuator.attenuator import BinaryFilterAttenuator
-from dodal.devices.backlight import Backlight, BacklightPosition
+from dodal.devices.backlight import Backlight
 from dodal.devices.dcm import DCM
 from dodal.devices.detector.detector_motion import DetectorMotion
 from dodal.devices.eiger import EigerDetector
@@ -18,7 +12,6 @@ from dodal.devices.fast_grid_scan import PandAFastGridScan, ZebraFastGridScan
 from dodal.devices.flux import Flux
 from dodal.devices.i03.beamstop import Beamstop
 from dodal.devices.oav.oav_detector import OAV
-from dodal.devices.oav.oav_parameters import OAVParameters
 from dodal.devices.oav.pin_image_recognition import PinTipDetection
 from dodal.devices.robot import BartRobot
 from dodal.devices.s4_slit_gaps import S4SlitGaps
@@ -35,30 +28,15 @@ from mx_bluesky.common.external_interaction.callbacks.common.grid_detection_call
     GridDetectionCallback,
     GridParamUpdate,
 )
-from mx_bluesky.common.external_interaction.callbacks.xray_centre.ispyb_callback import (
-    ispyb_activation_wrapper,
-)
 from mx_bluesky.common.parameters.constants import OavConstants
-from mx_bluesky.common.utils.context import device_composite_from_context
+from mx_bluesky.common.plans.common_grid_detect_then_xray_centre_plan import (
+    GridDetectThenXRayCentreComposite,
+    grid_detect_then_xray_centre,
+)
 from mx_bluesky.common.utils.log import LOGGER
-from mx_bluesky.common.xrc_result import XRayCentreEventHandler
-from mx_bluesky.hyperion.device_setup_plans.manipulate_sample import (
-    move_aperture_if_required,
-)
-from mx_bluesky.hyperion.device_setup_plans.utils import (
-    start_preparing_data_collection_then_do_plan,
-)
-from mx_bluesky.hyperion.experiment_plans.change_aperture_then_move_plan import (
-    change_aperture_then_move_to_xtal,
-)
 from mx_bluesky.hyperion.experiment_plans.flyscan_xray_centre_plan import (
     flyscan_xray_centre_no_move,
 )
-from mx_bluesky.hyperion.experiment_plans.oav_grid_detection_plan import (
-    OavGridDetectionComposite,
-    grid_detection_plan,
-)
-from mx_bluesky.hyperion.parameters.constants import CONST
 from mx_bluesky.hyperion.parameters.device_composites import (
     HyperionFlyScanXRayCentreComposite,
 )
@@ -69,7 +47,7 @@ from mx_bluesky.hyperion.parameters.gridscan import (
 
 
 @pydantic.dataclasses.dataclass(config={"arbitrary_types_allowed": True})
-class GridDetectThenXRayCentreComposite:
+class HyperionGridDetectThenXRayCentreComposite(GridDetectThenXRayCentreComposite):
     """All devices which are directly or indirectly required by this plan"""
 
     aperture_scatterguard: ApertureScatterguard
@@ -96,10 +74,6 @@ class GridDetectThenXRayCentreComposite:
     sample_shutter: ZebraShutter
 
 
-def create_devices(context: BlueskyContext) -> GridDetectThenXRayCentreComposite:
-    return device_composite_from_context(context, GridDetectThenXRayCentreComposite)
-
-
 def create_parameters_for_flyscan_xray_centre(
     grid_scan_with_edge_params: GridScanWithEdgeDetect,
     grid_parameters: GridParamUpdate,
@@ -111,62 +85,17 @@ def create_parameters_for_flyscan_xray_centre(
     return flyscan_xray_centre_parameters
 
 
-def detect_grid_and_do_gridscan(
-    composite: GridDetectThenXRayCentreComposite,
+def hyperion_grid_detect_then_xray_centre(
+    composite: HyperionGridDetectThenXRayCentreComposite,
     parameters: GridScanWithEdgeDetect,
-    oav_params: OAVParameters,
-):
-    snapshot_template = f"{parameters.detector_params.prefix}_{parameters.detector_params.run_number}_{{angle}}"
-
+    oav_config: str = OavConstants.OAV_CONFIG_JSON,
+) -> MsgGenerator:
+    """
+    A plan which combines the collection of snapshots from the OAV and the determination
+    of the grid dimensions to use for the following grid scan.
+    """
     grid_params_callback = GridDetectionCallback()
-
-    @bpp.subs_decorator([grid_params_callback])
-    def run_grid_detection_plan(
-        oav_params,
-        snapshot_template,
-        snapshot_dir: Path,
-    ):
-        grid_detect_composite = OavGridDetectionComposite(
-            backlight=composite.backlight,
-            oav=composite.oav,
-            smargon=composite.smargon,
-            pin_tip_detection=composite.pin_tip_detection,
-        )
-
-        yield from grid_detection_plan(
-            grid_detect_composite,
-            oav_params,
-            snapshot_template,
-            str(snapshot_dir),
-            parameters.grid_width_um,
-            parameters.box_size_um,
-        )
-
-    if parameters.selected_aperture:
-        # Start moving the aperture/scatterguard into position without moving it in
-        yield from bps.prepare(
-            composite.aperture_scatterguard,
-            parameters.selected_aperture,
-            group=CONST.WAIT.GRID_READY_FOR_DC,
-        )
-
-    yield from run_grid_detection_plan(
-        oav_params,
-        snapshot_template,
-        parameters.snapshot_directory,
-    )
-
-    yield from bps.abs_set(
-        composite.backlight, BacklightPosition.OUT, group=CONST.WAIT.GRID_READY_FOR_DC
-    )
-
-    yield from move_aperture_if_required(
-        composite.aperture_scatterguard,
-        parameters.selected_aperture,
-        group=CONST.WAIT.GRID_READY_FOR_DC,
-    )
-
-    yield from flyscan_xray_centre_no_move(
+    flyscan_xrc_plan = flyscan_xray_centre_no_move(
         HyperionFlyScanXRayCentreComposite(
             aperture_scatterguard=composite.aperture_scatterguard,
             attenuator=composite.attenuator,
@@ -191,52 +120,6 @@ def detect_grid_and_do_gridscan(
             parameters, grid_params_callback.get_grid_parameters()
         ),
     )
-
-
-def grid_detect_then_xray_centre(
-    composite: GridDetectThenXRayCentreComposite,
-    parameters: GridScanWithEdgeDetect,
-    oav_config: str = OavConstants.OAV_CONFIG_JSON,
-) -> MsgGenerator:
-    """
-    A plan which combines the collection of snapshots from the OAV and the determination
-    of the grid dimensions to use for the following grid scan.
-    """
-
-    eiger: EigerDetector = composite.eiger
-
-    eiger.set_detector_parameters(parameters.detector_params)
-
-    oav_params = OAVParameters("xrayCentring", oav_config)
-
-    flyscan_event_handler = XRayCentreEventHandler()
-
-    @subs_decorator(flyscan_event_handler)
-    def plan_to_perform():
-        yield from ispyb_activation_wrapper(
-            detect_grid_and_do_gridscan(
-                composite,
-                parameters,
-                oav_params,
-            ),
-            parameters,
-        )
-
-    yield from start_preparing_data_collection_then_do_plan(
-        composite.beamstop,
-        eiger,
-        composite.detector_motion,
-        parameters.detector_params.detector_distance,
-        plan_to_perform(),
-        group=CONST.WAIT.GRID_READY_FOR_DC,
-    )
-
-    assert flyscan_event_handler.xray_centre_results, (
-        "Flyscan result event not received or no crystal found and exception not raised"
-    )
-
-    yield from change_aperture_then_move_to_xtal(
-        flyscan_event_handler.xray_centre_results[0],
-        composite.smargon,
-        composite.aperture_scatterguard,
+    yield from grid_detect_then_xray_centre(
+        composite, parameters, flyscan_xrc_plan, oav_config
     )
