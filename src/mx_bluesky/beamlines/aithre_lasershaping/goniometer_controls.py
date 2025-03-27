d6b96b1634da8098c00daf19555f3d3e59720157
James Gilbert <james.gilbert@diamond.ac.uk>
2025-03-27 09:07:40 +0000

    Add plan and test for changing omega velocity

--- src/mx_bluesky/beamlines/aithre_lasershaping/goniometer_controls.py
+++ src/mx_bluesky/beamlines/aithre_lasershaping/goniometer_controls.py
@@ -3,6 +3,13 @@ from bluesky.utils import MsgGenerator
 from dodal.devices.aithre_lasershaping.goniometer import Goniometer
 
 
+def change_goniometer_turn_speed(
+    goniometer: Goniometer, velocity: float
+) -> MsgGenerator:
+    """Set the velocity of the goniometer"""
+    yield from bps.mv(goniometer.omega.velocity, velocity)
+
+
 def rotate_goniometer_relative(goniometer: Goniometer, value: float) -> MsgGenerator:
     """Adjust the goniometer position incrementally"""
     yield from bps.rel_set(goniometer.omega, value, wait=True)
