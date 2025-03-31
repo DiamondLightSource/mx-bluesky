77992140905928ffd83d4543ce4e4c4921eeccc8
James Gilbert <james.gilbert@diamond.ac.uk>
2025-03-31 13:19:34 +0100

    Add plan to rotate goniometer to furthest maximum

--- src/mx_bluesky/beamlines/aithre_lasershaping/goniometer_controls.py
+++ src/mx_bluesky/beamlines/aithre_lasershaping/goniometer_controls.py
@@ -5,7 +5,16 @@ from dodal.devices.aithre_lasershaping.goniometer import Goniometer
 
 
 def change_goniometer_turn_speed(
-    velocity: float, goniometer: Goniometer = inject("goniometer")
+    goniometer: Goniometer, velocity: float
 ) -> MsgGenerator:
     """Set the velocity of the goniometer"""
     yield from bps.mv(goniometer.omega.velocity, velocity)
+
+
+def go_to_furthest_maximum(
+    goniometer: Goniometer = inject("goniometer"),
+) -> MsgGenerator:
+    """Go to +/-3600, whichever is further away"""
+    current_value: float = yield from bps.rd(goniometer.omega.user_readback)
+
+    yield from bps.mv(goniometer.omega, -3600 if current_value > 0 else 3600)
