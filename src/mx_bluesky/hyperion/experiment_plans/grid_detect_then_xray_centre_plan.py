107ec979ad6ffce6b4689be747c22b40cad7c681
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2025-04-04 09:59:17 +0100

    Fix references to moved file

--- src/mx_bluesky/hyperion/experiment_plans/grid_detect_then_xray_centre_plan.py
+++ src/mx_bluesky/hyperion/experiment_plans/grid_detect_then_xray_centre_plan.py
@@ -31,6 +31,9 @@ from dodal.devices.zebra.zebra_controlled_shutter import ZebraShutter
 from dodal.devices.zocalo import ZocaloResults
 from ophyd_async.fastcs.panda import HDFPanda
 
+from mx_bluesky.common.device_setup_plans.manipulate_sample import (
+    move_aperture_if_required,
+)
 from mx_bluesky.common.experiment_plans.oav_grid_detection_plan import (
     OavGridDetectionComposite,
     grid_detection_plan,
@@ -47,9 +50,6 @@ from mx_bluesky.common.parameters.gridscan import GridCommon
 from mx_bluesky.common.utils.context import device_composite_from_context
 from mx_bluesky.common.utils.log import LOGGER
 from mx_bluesky.common.xrc_result import XRayCentreEventHandler
-from mx_bluesky.hyperion.device_setup_plans.manipulate_sample import (
-    move_aperture_if_required,
-)
 from mx_bluesky.hyperion.device_setup_plans.utils import (
     start_preparing_data_collection_then_do_plan,
 )
