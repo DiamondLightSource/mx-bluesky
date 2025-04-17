bf5cfbbc9380800e1a4b9510c2a048d7bcadfdb3
Dominic Oram <dominic.oram@diamond.ac.uk>
2025-04-17 18:00:13 +0100

    Fix test

--- tests/unit_tests/hyperion/external_interaction/test_agamemnon.py
+++ tests/unit_tests/hyperion/external_interaction/test_agamemnon.py
@@ -281,6 +281,7 @@ def test_if_failed_to_populate_parameters_from_hyperion_exception_is_logged(
 @patch("mx_bluesky.hyperion.external_interaction.agamemnon.LOGGER")
 @patch("mx_bluesky.hyperion.external_interaction.agamemnon.requests")
 @patch("mx_bluesky.common.parameters.components.os", new=MagicMock())
+@patch("dodal.devices.detector.detector.Path", new=MagicMock())
 def test_populate_parameters_from_agamemnon_causes_no_warning_when_compared_to_gda_params(
     mock_requests: MagicMock,
     mock_logger: MagicMock,
