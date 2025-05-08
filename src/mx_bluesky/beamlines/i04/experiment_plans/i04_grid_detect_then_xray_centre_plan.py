e47ef85ec48936b08efe92ed5c17bdf6e6411c81
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2025-05-08 12:52:28 +0100

    Add i04 grid detect plan

---
+++ src/mx_bluesky/beamlines/i04/experiment_plans/i04_grid_detect_then_xray_centre_plan.py
@@ -0,0 +1,110 @@
+from __future__ import annotations
+
+from functools import partial
+
+from blueapi.core import BlueskyContext
+from bluesky.utils import MsgGenerator
+from dodal.devices.fast_grid_scan import (
+    set_fast_grid_scan_params,
+)
+from dodal.plans.preprocessors.verify_undulator_gap import (
+    verify_undulator_gap_before_run_decorator,
+)
+
+from mx_bluesky.beamlines.i04.parameters.device_composites import (
+    I04FlyScanXRayCentreComposite,
+)
+from mx_bluesky.common.experiment_plans.common_flyscan_xray_centre_plan import (
+    BeamlineSpecificFGSFeatures,
+    construct_beamline_specific_FGS_features,
+)
+from mx_bluesky.common.experiment_plans.common_grid_detect_then_xray_centre_plan import (
+    grid_detect_then_xray_centre,
+)
+from mx_bluesky.common.parameters.constants import OavConstants
+from mx_bluesky.common.parameters.device_composites import (
+    GridDetectThenXRayCentreComposite,
+)
+from mx_bluesky.common.parameters.gridscan import GridCommon, SpecifiedThreeDGridScan
+from mx_bluesky.common.utils.context import device_composite_from_context
+from mx_bluesky.hyperion.experiment_plans.hyperion_flyscan_xray_centre_plan import (
+    _generic_tidy,
+    _zebra_triggering_setup,
+)
+from mx_bluesky.hyperion.parameters.device_composites import (
+    HyperionGridDetectThenXRayCentreComposite,
+)
+
+
+def create_devices(
+    context: BlueskyContext,
+) -> GridDetectThenXRayCentreComposite:
+    return device_composite_from_context(context, GridDetectThenXRayCentreComposite)
+
+
+def i04_grid_detect_then_xray_centre(
+    composite: HyperionGridDetectThenXRayCentreComposite,
+    parameters: GridCommon,
+    oav_config: str = OavConstants.OAV_CONFIG_JSON,
+) -> MsgGenerator:
+    """
+    A plan which combines the collection of snapshots from the OAV and the determination
+    of the grid dimensions to use for the following grid scan.
+    """
+
+    @verify_undulator_gap_before_run_decorator(composite)
+    def plan_to_perform():
+        yield from grid_detect_then_xray_centre(
+            composite=composite,
+            parameters=parameters,
+            xrc_params_type=SpecifiedThreeDGridScan,
+            construct_beamline_specific=construct_i04_specific_features,
+            oav_config=oav_config,
+        )
+
+    yield from plan_to_perform()
+
+
+def construct_i04_specific_features(
+    xrc_composite: I04FlyScanXRayCentreComposite,
+    xrc_parameters: SpecifiedThreeDGridScan,
+) -> BeamlineSpecificFGSFeatures:
+    """
+    Get all the information needed to do the i04 XRC flyscan.
+    """
+    signals_to_read_pre_flyscan = [
+        xrc_composite.undulator.current_gap,
+        xrc_composite.synchrotron.synchrotron_mode,
+        xrc_composite.s4_slit_gaps.xgap,
+        xrc_composite.s4_slit_gaps.ygap,
+        xrc_composite.smargon.x,
+        xrc_composite.smargon.y,
+        xrc_composite.smargon.z,
+        xrc_composite.dcm.energy_in_kev,
+    ]
+
+    signals_to_read_during_collection = [
+        xrc_composite.aperture_scatterguard,
+        xrc_composite.attenuator.actual_transmission,
+        xrc_composite.flux.flux_reading,
+        xrc_composite.dcm.energy_in_kev,
+        xrc_composite.eiger.bit_depth,
+    ]
+
+    setup_trigger_plan = _zebra_triggering_setup
+    tidy_plan = partial(_generic_tidy, group="flyscan_zebra_tidy", wait=True)
+    set_flyscan_params_plan = partial(
+        set_fast_grid_scan_params,
+        xrc_composite.zebra_fast_grid_scan,
+        xrc_parameters.FGS_params,
+    )
+    fgs_motors = xrc_composite.zebra_fast_grid_scan
+    return construct_beamline_specific_FGS_features(
+        setup_trigger_plan,
+        tidy_plan,
+        set_flyscan_params_plan,
+        fgs_motors,
+        signals_to_read_pre_flyscan,
+        signals_to_read_during_collection,
+        get_xrc_results_from_zocalo=True,
+    )
