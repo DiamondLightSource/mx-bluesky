44f53d26ac50eede81d71d756739ad0f40ec82e7
Robert Tuck <robert.tuck@diamond.ac.uk>
2025-05-12 16:39:09 +0100

    export BEAMLINE if it was set inside run_hyperion.sh

--- run_hyperion.sh
+++ run_hyperion.sh
@@ -12,6 +12,7 @@ for option in "$@"; do
     case $option in
         -b=*|--beamline=*)
             BEAMLINE="${option#*=}"
+            export BEAMLINE
             shift
             ;;
         --stop)
