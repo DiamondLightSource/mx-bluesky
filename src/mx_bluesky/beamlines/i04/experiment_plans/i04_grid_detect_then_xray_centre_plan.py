19b2199bab024c753a3c02f4c716d98114e5b5ac
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2025-05-14 15:15:04 +0100

    Add tidy plan

--- src/mx_bluesky/beamlines/i04/experiment_plans/i04_grid_detect_then_xray_centre_plan.py
+++ src/mx_bluesky/beamlines/i04/experiment_plans/i04_grid_detect_then_xray_centre_plan.py
@@ -5,15 +5,22 @@ from functools import partial
 import bluesky.preprocessors as bpp
 from blueapi.core import BlueskyContext
 from bluesky.utils import MsgGenerator
+from dodal.devices.aperturescatterguard import ApertureScatterguard
+from dodal.devices.backlight import Backlight
+from dodal.devices.detector.detector_motion import DetectorMotion
 from dodal.devices.fast_grid_scan import (
     set_fast_grid_scan_params,
 )
+from dodal.devices.smargon import Smargon
 from dodal.plans.preprocessors.verify_undulator_gap import (
     verify_undulator_gap_before_run_decorator,
 )
 
 from mx_bluesky.beamlines.i04.parameters.device_composites import (
     I04FlyScanXRayCentreComposite,
+)
+from mx_bluesky.common.device_setup_plans.manipulate_sample import (
+    cleanup_sample_environment,
 )
 from mx_bluesky.common.experiment_plans.common_flyscan_xray_centre_plan import (
     BeamlineSpecificFGSFeatures,
@@ -60,6 +67,7 @@ def i04_grid_detect_then_xray_centre(
     composite: GridDetectThenXRayCentreComposite,
     parameters: GridCommon,
     oav_config: str = OavConstants.OAV_CONFIG_JSON,
+    udc: bool = False,
 ) -> MsgGenerator:
     """
     An plan which combines the collection of snapshots from the OAV and the determination
@@ -83,6 +91,24 @@ def i04_grid_detect_then_xray_centre(
         )
 
     yield from grid_detect_then_xray_centre_with_callbacks()
+
+    if not udc:
+        yield from get_ready_for_oav_and_close_shutter(
+            composite.smargon,
+            composite.backlight,
+            composite.aperture_scatterguard,
+            composite.detector_motion,
+        )
+
+
+def get_ready_for_oav_and_close_shutter(
+    smargon: Smargon,
+    backlight: Backlight,
+    aperture_scatterguard: ApertureScatterguard,
+    detector_motion: DetectorMotion,
+):
+    yield from setup_beamline_for_OAV(smargon, backlight, aperture_scatterguard)
+    yield from cleanup_sample_environment(detector_motion)
 
 
 def create_gridscan_callbacks() -> tuple[
