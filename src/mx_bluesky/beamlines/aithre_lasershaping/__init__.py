7e0ab2e0f8a3272d883fad9f5fb288e700f8f741
James Gilbert <james.gilbert@diamond.ac.uk>
2025-05-19 15:37:27 +0100

    Changes from the lab

--- src/mx_bluesky/beamlines/aithre_lasershaping/__init__.py
+++ src/mx_bluesky/beamlines/aithre_lasershaping/__init__.py
@@ -1,3 +1,4 @@
+from mx_bluesky.beamlines.aithre_lasershaping.beamline_safe import check_beamline_safe
 from mx_bluesky.beamlines.aithre_lasershaping.check_goniometer_performance import (
     check_omega_performance,
 )
@@ -7,6 +8,7 @@ from mx_bluesky.beamlines.aithre_lasershaping.goniometer_controls import (
 )
 
 __all__ = [
+    "check_beamline_safe",
     "check_omega_performance",
     "change_goniometer_turn_speed",
     "rotate_goniometer_relative",
--- src/mx_bluesky/beamlines/aithre_lasershaping/beamline_safe.py
+++ src/mx_bluesky/beamlines/aithre_lasershaping/beamline_safe.py
@@ -1,10 +1,13 @@
 import bluesky.plan_stubs as bps
 from bluesky.utils import MsgGenerator
+from dodal.common import inject
 from dodal.devices.aithre_lasershaping.goniometer import Goniometer
 from dodal.devices.aithre_lasershaping.laser_robot import BeamlineSafe, LaserRobot
 
 
-def check_beamline_safe(robot: LaserRobot, goniometer: Goniometer) -> MsgGenerator:
+def check_beamline_safe(
+    robot: LaserRobot = inject("robot"), goniometer: Goniometer = inject("goniometer")
+) -> MsgGenerator:
     pvs = [
         goniometer.x,
         goniometer.y,
@@ -16,11 +19,12 @@ def check_beamline_safe(robot: LaserRobot, goniometer: Goniometer) -> MsgGenerat
 
     values: list[float] = []
     for i in range(len(pvs)):
-        values[i] = yield from bps.rd(pvs[i])
+        values.append((yield from bps.rd(pvs[i])))
 
     set_value = (
-        BeamlineSafe.ON
+        BeamlineSafe.ON.value
         if all(round(value, 3) == 0 for value in values)
-        else BeamlineSafe.NO
+        else BeamlineSafe.NO.value
     )
     yield from bps.abs_set(robot.set_beamline_safe, set_value, wait=True)
+    return values
