a284435d43aebc27a0977b4882fbbd5d334bd07e
JamesDoingStuff <JamesDoingStuff@users.noreply.github.com>
2025-06-03 13:11:55 +0000

    deploy: b5dfce71daa5a5ef0f35b0e7c44a806a3fcce48d

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.0.1.dev264+g7f089c4d8',
+    VERSION: '1.0.1.dev265+gb5dfce71d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
