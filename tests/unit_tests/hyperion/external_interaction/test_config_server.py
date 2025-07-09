75a8f75cb98e60034eb4bd6d2385390aac0c2673
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2025-07-09 13:16:58 +0100

    tidy test

--- tests/unit_tests/hyperion/external_interaction/test_config_server.py
+++ tests/unit_tests/hyperion/external_interaction/test_config_server.py
@@ -1,9 +1,19 @@
 from mx_bluesky.hyperion.external_interaction.config_server import (
     get_hyperion_config_server,
 )
+from mx_bluesky.hyperion.parameters.constants import HyperionFeatureFlags
 
 
 def test_get_feature_flags():
     server = get_hyperion_config_server()
     features = server.get_feature_flags()
-    assert not features.USE_PANDA_FOR_GRIDSCAN
+
+    expected_features_dict = {
+        "USE_GPU_RESULTS": True,
+        "USE_PANDA_FOR_GRIDSCAN": False,
+        "SET_STUB_OFFSETS": False,
+    }
+
+    expected_features = HyperionFeatureFlags(**expected_features_dict)
+
+    assert features == expected_features
