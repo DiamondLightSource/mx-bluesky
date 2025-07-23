2a3866e26bc1eb4af7922578b64652cdf40803dc
Robert Tuck <robert.tuck@diamond.ac.uk>
2025-07-23 10:13:02 +0100

    Mark system test as requiring graylog and not part of nightly tests, change graylog port for test

--- tests/system_tests/hyperion/external_interaction/alerting/test_log_based_alerts.py
+++ tests/system_tests/hyperion/external_interaction/alerting/test_log_based_alerts.py
@@ -6,12 +6,13 @@ from dodal.log import get_graylog_configuration, set_up_graylog_handler
 from mx_bluesky.common.external_interaction.alerting.log_based_service import (
     LoggingAlertService,
 )
+from mx_bluesky.hyperion.parameters.constants import HyperionConstants
 
 
-@pytest.mark.system_test
+@pytest.mark.requires(external="graylog")
 def test_alert_to_graylog():
     logger = logging.getLogger("Dodal")
-    host, port = get_graylog_configuration(False)
-    set_up_graylog_handler(logger, host, port)
+    host, _ = get_graylog_configuration(False)
+    set_up_graylog_handler(logger, host, HyperionConstants.GRAYLOG_PORT)
     alert_service = LoggingAlertService()
     alert_service.raise_alert("Test alert", "This is a test.", {"alert_type": "Test"})
