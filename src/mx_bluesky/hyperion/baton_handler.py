a686577577eda790e0eaf41b2c4256ed7086342e
Robert Tuck <robert.tuck@diamond.ac.uk>
2025-08-08 13:24:02 +0100

    Add missing unit test

--- src/mx_bluesky/hyperion/baton_handler.py
+++ src/mx_bluesky/hyperion/baton_handler.py
@@ -6,6 +6,7 @@ from blueapi.core.context import BlueskyContext
 from bluesky import plan_stubs as bps
 from bluesky import preprocessors as bpp
 from bluesky.utils import MsgGenerator, RunEngineInterrupted
+from dodal.common.beamlines.commissioning_mode import set_commissioning_signal
 from dodal.devices.baton import Baton
 from dodal.devices.xbpm_feedback import XBPMFeedback
 
@@ -81,6 +82,7 @@ def run_udc_when_requested(context: BlueskyContext, runner: PlanRunner):
 
     def acquire_baton() -> MsgGenerator:
         yield from _wait_for_hyperion_requested(baton)
+        LOGGER.debug("Hyperion is now current baton holder.")
         yield from bps.abs_set(baton.current_user, HYPERION_USER)
 
     def collect() -> MsgGenerator:
@@ -108,14 +110,16 @@ def run_udc_when_requested(context: BlueskyContext, runner: PlanRunner):
         # user so that hyperion doesn't think we're requested again
         baton = _get_baton(context)
         previous_requested_user = yield from _safely_release_baton(baton)
+        LOGGER.debug("Hyperion no longer current baton holder.")
         yield from bps.abs_set(baton.current_user, NO_USER, wait=True)
         _raise_baton_released_alert(get_alerting_service(), previous_requested_user)
 
     def collect_then_release() -> MsgGenerator:
         xbpm_feedback = find_device_in_context(context, "xbpm_feedback", XBPMFeedback)
         yield from feedback_wrapper_for_commissioning_mode(
-            xbpm_feedback, baton,
-            bpp.contingency_wrapper(collect(), final_plan=release_baton)
+            xbpm_feedback,
+            baton,
+            bpp.contingency_wrapper(collect(), final_plan=release_baton),
         )
 
     context.run_engine(acquire_baton())
@@ -133,14 +137,18 @@ def _initialise_udc(context: BlueskyContext):
     """
     LOGGER.info("Initialising mx-bluesky for UDC start...")
     clear_all_device_caches(context)
+    LOGGER.debug("Reinitialising beamline devices")
     setup_devices(context, False)
+    set_commissioning_signal(_get_baton(context).commissioning)
 
 
 def _wait_for_hyperion_requested(baton: Baton):
+    LOGGER.debug("Hyperion waiting for baton...")
     SLEEP_PER_CHECK = 0.1
     while True:
         requested_user = yield from bps.rd(baton.requested_user)
         if requested_user == HYPERION_USER:
+            LOGGER.debug("Baton requested for Hyperion")
             break
         yield from bps.sleep(SLEEP_PER_CHECK)
 
@@ -222,6 +230,7 @@ def _safely_release_baton(baton: Baton) -> MsgGenerator[str]:
     """
     requested_user = yield from bps.rd(baton.requested_user)
     if requested_user == HYPERION_USER:
+        LOGGER.debug("Hyperion no longer requesting baton")
         yield from bps.abs_set(baton.requested_user, NO_USER)
         return NO_USER
     return requested_user
