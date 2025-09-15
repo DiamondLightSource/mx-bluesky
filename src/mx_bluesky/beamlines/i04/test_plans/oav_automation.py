7e0f0a1e9ecf015bdbcabb626e3aad40f0f35b2e
Srishty <srish.saj@gmail.com>
2025-09-15 15:31:32 +0100

    add transmission percentage

---
+++ src/mx_bluesky/beamlines/i04/test_plans/oav_automation.py
@@ -0,0 +1,30 @@
+
+
+import bluesky.plan_stubs as bps
+import bluesky.preprocessors as bpp
+#from bluesky.preprocessors import run_decorator, stage_wrapper, subs_decorator
+from bluesky.utils import MsgGenerator
+
+#from dodal.devices
+
+"""
+My task: 
+
+    Move the scintillator in and out
+    Change transmission percentage
+    Take OAV images
+    Open and close fast shutter
+
+"""
+
+def move_scintillator():
+    pass
+
+def set_transmission_percentage():
+    pass 
+
+def take_OAV_image():
+    pass 
+
+def open_close_fast_shutter():
+    pass
