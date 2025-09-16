437f52293ab5ba3e2e4b5623e8cddcd3ceb6c7a8
Sze Ching <teo.ching@diamond.ac.uk>
2025-09-16 16:41:38 +0100

    Revert "Remove get-xyz-limits"
    This reverts commit b3ed6a76ecd7f594950788ee07185d10c4234fdb.

--- src/mx_bluesky/hyperion/device_setup_plans/smargon.py
+++ src/mx_bluesky/hyperion/device_setup_plans/smargon.py
@@ -8,7 +8,13 @@ from mx_bluesky.common.utils.exceptions import SampleException
 def move_smargon_warn_on_out_of_range(
     smargon: Smargon, position: np.ndarray | list[float] | tuple[float, float, float]
 ):
-    """Moves Smargon to the specific position. Motor limits are handled by ophyd_async"""
+    """Throws a SampleException if the specified position is out of range for the
+    smargon. Otherwise moves to that position."""
+    limits = yield from smargon.get_xyz_limits()
+    if not limits.position_valid(position):
+        raise SampleException(
+            "Pin tip centring failed - pin too long/short/bent and out of range"
+        )
     yield from bps.mv(
         smargon, CombinedMove(x=position[0], y=position[1], z=position[2])
     )
