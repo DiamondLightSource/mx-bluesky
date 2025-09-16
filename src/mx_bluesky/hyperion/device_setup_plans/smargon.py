b1f4deb7a7cadc662b486b5d479e1b5961bf475c
Sze Ching <teo.ching@diamond.ac.uk>
2025-09-16 16:53:55 +0100

    Try to catch the MotorLimitsException and re-raise a SampleException

--- src/mx_bluesky/hyperion/device_setup_plans/smargon.py
+++ src/mx_bluesky/hyperion/device_setup_plans/smargon.py
@@ -1,6 +1,7 @@
 import numpy as np
 from bluesky import plan_stubs as bps
 from dodal.devices.smargon import CombinedMove, Smargon
+from ophyd_async.epics.motor import MotorLimitsException
 
 from mx_bluesky.common.utils.exceptions import SampleException
 
@@ -9,12 +10,12 @@ def move_smargon_warn_on_out_of_range(
     smargon: Smargon, position: np.ndarray | list[float] | tuple[float, float, float]
 ):
     """Throws a SampleException if the specified position is out of range for the
-    smargon. Otherwise moves to that position."""
-    limits = yield from smargon.get_xyz_limits()
-    if not limits.position_valid(position):
-        raise SampleException(
+    smargon. Otherwise moves to that position. The check is from ophyd-async"""
+    try:
+        yield from bps.mv(
+            smargon, CombinedMove(x=position[0], y=position[1], z=position[2])
+        )
+    except Exception:
+        raise SampleException from MotorLimitsException(
             "Pin tip centring failed - pin too long/short/bent and out of range"
         )
-    yield from bps.mv(
-        smargon, CombinedMove(x=position[0], y=position[1], z=position[2])
-    )
