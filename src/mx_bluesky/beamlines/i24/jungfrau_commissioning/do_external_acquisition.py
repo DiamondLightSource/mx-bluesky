824a2c2300dc2d523509c60591f9285a33e35ee8
Ollie Silvester <oliver.silvester@diamond.ac.uk>
2025-09-17 13:51:18 +0000

    Updates from tests

--- src/mx_bluesky/beamlines/i24/jungfrau_commissioning/do_external_acquisition.py
+++ src/mx_bluesky/beamlines/i24/jungfrau_commissioning/do_external_acquisition.py
@@ -21,7 +21,6 @@ from mx_bluesky.common.utils.log import LOGGER
 def do_external_acquisition(
     exp_time_s: float,
     total_triggers: PositiveInt = 1,
-    period_between_frames_s: float | None = None,
     jungfrau: Jungfrau = inject("jungfrau"),
     path_of_output_file: str | None = None,
     wait: bool = False,
@@ -34,7 +33,6 @@ def do_external_acquisition(
     Args:
         exp_time_s: Length of detector exposure for each frame.
         total_triggers: Number of external triggers recieved before acquisition is marked as complete.
-        period_between_frames_s: Time between each detector frame, including deadtime. Not needed if frames_per_triggers is 1.
         jungfrau: Jungfrau device
         path_of_output_file: Absolute path of the detector file output, including file name. If None, then use the PathProvider
             set during jungfrau device instantiation
@@ -49,7 +47,8 @@ def do_external_acquisition(
         jungfrau._writer._path_provider = path_provider  # noqa: SLF001
 
     trigger_info = create_jungfrau_external_triggering_info(
-        total_triggers, exp_time_s, period_between_frames_s
+        total_triggers,
+        exp_time_s,
     )
     yield from bps.stage(jungfrau)
     LOGGER.info("Setting up detector...")
