9ed8820f3f8894af455584b09bf7c52df6edd7ec
Sze Ching <teo.ching@diamond.ac.uk>
2025-09-22 15:48:35 +0100

    Remove backlight in test

--- tests/unit_tests/hyperion/experiment_plans/test_rotation_scan_plan.py
+++ tests/unit_tests/hyperion/experiment_plans/test_rotation_scan_plan.py
@@ -551,13 +551,6 @@ def test_rotation_snapshot_setup_called_to_move_backlight_in_aperture_out_before
     msgs = assert_message_and_return_remaining(
         rotation_scan_simulated_messages,
         lambda msg: msg.command == "set"
-        and msg.obj.name == "backlight"
-        and msg.args[0] == InOut.IN
-        and msg.kwargs["group"] == CONST.WAIT.READY_FOR_OAV,
-    )
-    msgs = assert_message_and_return_remaining(
-        msgs,
-        lambda msg: msg.command == "set"
         and msg.obj.name == "aperture_scatterguard-selected_aperture"
         and msg.args[0] == ApertureValue.OUT_OF_BEAM
         and msg.kwargs["group"] == CONST.WAIT.READY_FOR_OAV,
