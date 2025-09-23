778db9792ae44d34b3b5e745fa7864f594f1f0c1
Sze Ching <teo.ching@diamond.ac.uk>
2025-09-23 11:25:24 +0100

    Generalize move_smargon to move_xyzomegastage

--- src/mx_bluesky/hyperion/device_setup_plans/smargon.py
+++ src/mx_bluesky/hyperion/device_setup_plans/smargon.py
@@ -1,6 +1,7 @@
 import numpy as np
 from bluesky import plan_stubs as bps
 from bluesky.utils import FailedStatus
+from dodal.devices.motor import XYZOmegaStage
 from dodal.devices.smargon import CombinedMove, Smargon
 from ophyd_async.epics.motor import MotorLimitsException
 
@@ -16,6 +17,42 @@ def move_smargon_warn_on_out_of_range(
         yield from bps.mv(
             smargon, CombinedMove(x=position[0], y=position[1], z=position[2])
         )
+    except FailedStatus as fs:
+        if isinstance(fs.__cause__, MotorLimitsException):
+            raise SampleException(
+                "Pin tip centring failed - pin too long/short/bent and out of range"
+            ) from fs.__cause__
+        else:
+            raise fs
+
+
+def move_xyzomegastage_warn_on_out_of_range(
+    xyzomegastage: XYZOmegaStage,
+    position: np.ndarray | list[float] | tuple[float, float, float],
+):
+    """General version of move_smargon_warn_on_out_of_range"""
+    try:
+        yield from bps.mv(xyzomegastage.x, position[0])
+    except FailedStatus as fs:
+        if isinstance(fs.__cause__, MotorLimitsException):
+            raise SampleException(
+                "Pin tip centring failed - pin too long/short/bent and out of range"
+            ) from fs.__cause__
+        else:
+            raise fs
+
+    try:
+        yield from bps.mv(xyzomegastage.y, position[1])
+    except FailedStatus as fs:
+        if isinstance(fs.__cause__, MotorLimitsException):
+            raise SampleException(
+                "Pin tip centring failed - pin too long/short/bent and out of range"
+            ) from fs.__cause__
+        else:
+            raise fs
+
+    try:
+        yield from bps.mv(xyzomegastage.z, position[2])
     except FailedStatus as fs:
         if isinstance(fs.__cause__, MotorLimitsException):
             raise SampleException(
