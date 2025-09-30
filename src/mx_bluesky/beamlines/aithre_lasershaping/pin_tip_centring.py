b3cb51b4442d5d6e98ca45110e5f37284d45a061
Sze Ching <teo.ching@diamond.ac.uk>
2025-09-30 16:13:08 +0100

    Update aithre entry point

--- src/mx_bluesky/beamlines/aithre_lasershaping/pin_tip_centring.py
+++ src/mx_bluesky/beamlines/aithre_lasershaping/pin_tip_centring.py
@@ -1,10 +1,11 @@
 from bluesky.utils import MsgGenerator
 from dodal.common import inject
+from dodal.devices.aithre_lasershaping.goniometer import Goniometer
 from dodal.devices.backlight import Backlight
-from dodal.devices.motors import XYZOmegaStage
 from dodal.devices.oav.oav_detector import OAV
 from dodal.devices.oav.pin_image_recognition import PinTipDetection
 
+from mx_bluesky.beamlines.aithre_lasershaping.parameters.constants import CONST
 from mx_bluesky.common.experiment_plans.pin_tip_centring_plan import (
     PinTipCentringComposite,
     pin_tip_centre_plan,
@@ -14,10 +15,10 @@ from mx_bluesky.common.experiment_plans.pin_tip_centring_plan import (
 def aithre_pin_tip_centre(
     backlight: Backlight = inject("backlight"),
     oav: OAV = inject("OAV"),
-    gonio: XYZOmegaStage = inject("gonio"),
+    gonio: Goniometer = inject("gonio"),
     pin_tip_detection: PinTipDetection = inject("pin_tip_detection"),
     tip_offset_microns: float = inject("tip_offset_microns"),
-    oav_config_file: str = inject("oav_config_file"),
+    oav_config_file: str = CONST.OAV_CENTRING_FILE,
 ) -> MsgGenerator:
     """
     A plan that use pin_tip_centre_plan from common for aithre
