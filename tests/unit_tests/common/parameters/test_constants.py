54995504c5a169f4f4ea8c6e9fce5f8d3808962c
Sze Ching <teo.ching@diamond.ac.uk>
2025-10-01 14:47:52 +0100

    Fix test

--- tests/unit_tests/common/parameters/test_constants.py
+++ tests/unit_tests/common/parameters/test_constants.py
@@ -15,9 +15,9 @@ from mx_bluesky.common.parameters.constants import _get_oav_config_json_path
             "/dls_sw/i03/software/daq_configuration/json/OAVCentring_hyperion.json",
         ),
         ("i04", False, "/dls_sw/i04/software/daq_configuration/json/OAVCentring.json"),
-        ("AITHRE", True, "tests/test_data/test_OAVCentring.json"),
+        ("aithre", True, "tests/test_data/test_OAVCentring.json"),
         (
-            "AITHRE",
+            "aithre",
             False,
             "/dls/science/groups/i23/aithre/daq_configuration/json/OAVCentring_aithre.json",
         ),
