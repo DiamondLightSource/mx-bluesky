ffa4c37ebfe55d5aa111caf432107ce5a69edcee
Sze Ching <teo.ching@diamond.ac.uk>
2025-10-07 13:40:15 +0100

    Hardcode aithre path

--- src/mx_bluesky/beamlines/aithre_lasershaping/parameters/constants.py
+++ src/mx_bluesky/beamlines/aithre_lasershaping/parameters/constants.py
@@ -2,9 +2,9 @@ import os
 
 from pydantic.dataclasses import dataclass
 
-from mx_bluesky.common.parameters.constants import (
-    OavConstants,
-)
+# from mx_bluesky.common.parameters.constants import (
+#    OavConstants,
+# )
 
 TEST_MODE = os.environ.get("AITHRE_TEST_MODE")
 
@@ -12,7 +12,10 @@ TEST_MODE = os.environ.get("AITHRE_TEST_MODE")
 @dataclass(frozen=True)
 class AithreConstants:
     BEAMLINE = "aithre"
-    OAV_CENTRING_FILE = OavConstants.OAV_CONFIG_JSON
+    #    OAV_CENTRING_FILE = OavConstants.OAV_CONFIG_JSON
+    OAV_CENTRING_FILE = (
+        "/dls/science/groups/i23/aithre/daq_configuration/json/OAVCentring_aithre.json"
+    )
     LOG_FILE_NAME = "aithre.log"
 
 
