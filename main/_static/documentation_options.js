249b50153f69af7005772086b41b2dbeaf7f2a6b
noemifrisina <noemifrisina@users.noreply.github.com>
2025-10-27 16:05:52 +0000

    deploy: ad47f80ad1debee57b5fb6de94874f1945cb63db

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.0.1.dev381+g17d9beedf',
+    VERSION: '1.0.1.dev382+gad47f80ad',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
