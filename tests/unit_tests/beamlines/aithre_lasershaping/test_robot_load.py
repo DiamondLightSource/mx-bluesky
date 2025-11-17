420241b6a1d1e84bd9efd96ba9740ac3a4c0b66b
Sze Ching <teo.ching@diamond.ac.uk>
2025-11-17 17:20:01 +0000

    Try fixing test

--- tests/unit_tests/beamlines/aithre_lasershaping/test_robot_load.py
+++ tests/unit_tests/beamlines/aithre_lasershaping/test_robot_load.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from unittest.mock import MagicMock, patch
+from unittest.mock import ANY, MagicMock, patch
 
 import pytest
 from bluesky.run_engine import RunEngine
@@ -12,6 +12,7 @@ from mx_bluesky.beamlines.aithre_lasershaping.parameters.robot_load_parameters i
 from mx_bluesky.beamlines.aithre_lasershaping.robot_load_plan import (
     RobotLoadComposite,
     robot_load_and_snapshots_plan,
+    robot_unload_plan,
     take_robot_snapshots,
 )
 from mx_bluesky.hyperion.external_interaction.callbacks.robot_actions.ispyb_callback import (
@@ -88,58 +89,6 @@ def test_given_ispyb_callback_attached_when_robot_load_and_snapshots_plan_called
 
 
 # Leave for later load and centre plan
-"""
-@patch(
-    "mx_bluesky.hyperion.external_interaction.callbacks.robot_actions.ispyb_callback.ExpeyeInteraction"
-)
-@patch(
-    "mx_bluesky.hyperion.experiment_plans.robot_load_and_change_energy.set_energy_plan",
-    MagicMock(return_value=iter([])),
-)
-def test_given_ispyb_callback_attached_when_robot_load_then_centre_plan_called_then_ispyb_deposited(
-    exp_eye: MagicMock,
-    robot_load_and_energy_change_composite: RobotLoadAndEnergyChangeComposite,
-    robot_load_and_energy_change_params: RobotLoadAndEnergyChange,
-    run_engine: RunEngine,
-):
-    robot = robot_load_and_energy_change_composite.robot
-    webcam = robot_load_and_energy_change_composite.webcam
-    set_mock_value(
-        robot_load_and_energy_change_composite.oav.snapshot.last_saved_path,
-        "test_oav_snapshot",
-    )
-    set_mock_value(webcam.last_saved_path, "test_webcam_snapshot")
-    webcam.trigger = MagicMock(return_value=NullStatus())
-    set_mock_value(robot.barcode, "BARCODE")
-
-    run_engine.subscribe(RobotLoadISPyBCallback())
-
-    action_id = 1098
-    exp_eye.return_value.start_robot_action.return_value = action_id
-
-    run_engine(
-        robot_load_and_change_energy_plan(
-            robot_load_and_energy_change_composite, robot_load_and_energy_change_params
-        )
-    )
-
-    exp_eye.return_value.start_robot_action.assert_called_once_with(
-        "LOAD", "cm31105", 4, 12345
-    )
-    exp_eye.return_value.update_robot_action.assert_called_once_with(
-        action_id,
-        {
-            "sampleBarcode": "BARCODE",
-            "xtalSnapshotBefore": "test_webcam_snapshot",
-            "xtalSnapshotAfter": "test_oav_snapshot",
-            "containerLocation": 3,
-            "dewarLocation": 40,
-        },
-    )
-    exp_eye.return_value.end_robot_action.assert_called_once_with(
-        action_id, "success", "OK"
-    )
-"""
 
 
 @patch("mx_bluesky.beamlines.aithre_lasershaping.robot_load_plan.datetime")
@@ -157,3 +106,102 @@ async def test_when_take_snapshots_called_then_filename_and_directory_set_and_de
     oav.snapshot.trigger.assert_called_once()
     assert await oav.snapshot.filename.get_value() == "TIME_oav-snapshot_after_load"
     assert await oav.snapshot.directory.get_value() == test_directory
+
+
+@patch(
+    "mx_bluesky.beamlines.aithre_lasershaping.robot_load_plan.move_gonio_to_home_position",
+    autospec=True,
+)
+async def test_when_robot_unload_called_then_sample_area_prepared_before_load(
+    mock_move_gonio,
+    aithre_robot_load_composite: RobotLoadComposite,
+    robot_load_params: AithreRobotLoad,
+    run_engine: RunEngine,
+):
+    run_engine(robot_unload_plan(aithre_robot_load_composite, robot_load_params))
+
+    mock_move_gonio.assert_called_once()
+
+
+def test_when_unload_plan_run_then_initial_unload_ispyb_deposition_made(
+    aithre_robot_load_composite: RobotLoadComposite,
+    robot_load_params: AithreRobotLoad,
+    run_engine: RunEngine,
+):
+    callback = RobotLoadISPyBCallback()
+    callback.expeye = (mock_expeye := MagicMock())
+    run_engine.subscribe(callback)
+
+    set_mock_value(
+        aithre_robot_load_composite.robot.sample_id, expected_sample_id := 12345
+    )
+
+    run_engine(robot_unload_plan(aithre_robot_load_composite, robot_load_params))
+
+    mock_expeye.start_robot_action.assert_called_once_with(
+        "UNLOAD", "cm31105", 4, expected_sample_id
+    )
+
+
+def test_when_unload_plan_run_then_full_ispyb_deposition_made(
+    aithre_robot_load_composite: RobotLoadComposite,
+    robot_load_params: AithreRobotLoad,
+    run_engine: RunEngine,
+):
+    callback = RobotLoadISPyBCallback()
+    callback.expeye = (mock_expeye := MagicMock())
+    run_engine.subscribe(callback)
+
+    set_mock_value(
+        aithre_robot_load_composite.robot.sample_id, expected_sample_id := 12345
+    )
+    set_mock_value(aithre_robot_load_composite.robot.current_pin, expected_pin := 3)
+    set_mock_value(aithre_robot_load_composite.robot.current_puck, expected_puck := 40)
+    set_mock_value(
+        aithre_robot_load_composite.robot.barcode, expected_barcode := "BARODE"
+    )
+
+    action_id = 1098
+    mock_expeye.start_robot_action.return_value = action_id
+
+    run_engine(robot_unload_plan(aithre_robot_load_composite, robot_load_params))
+
+    mock_expeye.start_robot_action.assert_called_once_with(
+        "UNLOAD", "cm31105", 4, expected_sample_id
+    )
+    mock_expeye.update_robot_action.assert_called_once_with(
+        action_id,
+        {
+            "sampleBarcode": expected_barcode,
+            "containerLocation": expected_pin,
+            "dewarLocation": expected_puck,
+        },
+    )
+    mock_expeye.end_robot_action.assert_called_once_with(action_id, "success", "OK")
+
+
+@patch(
+    "mx_bluesky.beamlines.aithre_lasershaping.robot_load_plan.move_gonio_to_home_position",
+    autospec=True,
+)
+def test_when_unload_plan_fails_then_error_deposited_in_ispyb(
+    mock_move_gonio: MagicMock,
+    aithre_robot_load_composite: RobotLoadComposite,
+    robot_load_params: AithreRobotLoad,
+    run_engine: RunEngine,
+):
+    class TestError(Exception): ...
+
+    callback = RobotLoadISPyBCallback()
+    callback.expeye = (mock_expeye := MagicMock())
+    run_engine.subscribe(callback)
+    mock_move_gonio.side_effect = TestError("Bad Error")
+
+    action_id = 1098
+    mock_expeye.start_robot_action.return_value = action_id
+
+    with pytest.raises(TestError):
+        run_engine(robot_unload_plan(aithre_robot_load_composite, robot_load_params))
+
+    mock_expeye.start_robot_action.assert_called_once_with("UNLOAD", "cm31105", 4, ANY)
+    mock_expeye.end_robot_action.assert_called_once_with(action_id, "fail", "Bad Error")
