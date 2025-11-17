0e0a96061d63f479f2ee4136fd1aa8bc76b75de4
Sze Ching <teo.ching@diamond.ac.uk>
2025-11-17 13:34:11 +0000

    Fix tests

--- tests/unit_tests/beamlines/aithre_lasershaping/test_aithre_pin_tip_centre.py
+++ tests/unit_tests/beamlines/aithre_lasershaping/test_aithre_pin_tip_centre.py
@@ -1,13 +1,10 @@
-from functools import partial
-from unittest.mock import AsyncMock, MagicMock, patch
+from unittest.mock import MagicMock, patch
 
-import pytest
 from bluesky.plan_stubs import null
 from bluesky.run_engine import RunEngine
 from dodal.devices.aithre_lasershaping.goniometer import Goniometer
 from dodal.devices.oav.oav_detector import OAV
 from dodal.devices.oav.pin_image_recognition import PinTipDetection
-from ophyd_async.testing import set_mock_value
 
 from mx_bluesky.beamlines.aithre_lasershaping.pin_tip_centring import (
     aithre_pin_tip_centre,
@@ -19,31 +16,26 @@ def return_pixel(pixel, *args):
     return pixel
 
 
-@pytest.fixture
-def mock_pin_tip(pin_tip: PinTipDetection):
-    pin_tip._get_tip_and_edge_data = AsyncMock(return_value=pin_tip.INVALID_POSITION)
-    return pin_tip
-
-
 @patch(
     "mx_bluesky.beamlines.aithre_lasershaping.pin_tip_centring.pin_tip_centre_plan",
     autospec=True,
 )
 async def test_when_aithre_pin_tip_centre_called_then_expected_plans_called(
     mock_pin_tip_centring_plan,
-    gonio_with_limits: Goniometer,
+    aithre_gonio: Goniometer,
     oav: OAV,
     test_config_files: dict[str, str],
     run_engine: RunEngine,
 ):
+    mock_pin_tip_detection = MagicMock(spec=PinTipDetection)
     run_engine(
         aithre_pin_tip_centre(
             oav,
-            gonio_with_limits,
+            aithre_gonio,
             mock_pin_tip_detection,
             50,
             test_config_files["oav_config_json"],
         )
     )
 
-    assert mock_pin_tip_centring_plan.assert_called_once()
+    mock_pin_tip_centring_plan.assert_called_once()
