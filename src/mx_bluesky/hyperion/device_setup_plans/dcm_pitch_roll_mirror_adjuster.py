989e579ca529086a48fe34030c4e9bca5747b9d1
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2025-12-09 09:05:36 +0000

    Use config server to read pitch and roll config

--- src/mx_bluesky/hyperion/device_setup_plans/dcm_pitch_roll_mirror_adjuster.py
+++ src/mx_bluesky/hyperion/device_setup_plans/dcm_pitch_roll_mirror_adjuster.py
@@ -1,6 +1,8 @@
 import json
 
 import bluesky.plan_stubs as bps
+from daq_config_server.client import ConfigServer
+from daq_config_server.converters.models import GenericLookupTable
 from dodal.devices.focusing_mirror import (
     FocusingMirrorWithStripes,
     MirrorStripe,
@@ -10,7 +12,6 @@ from dodal.devices.i03.undulator_dcm import UndulatorDCM
 from dodal.devices.util.adjuster_plans import lookup_table_adjuster
 from dodal.devices.util.lookup_tables import (
     linear_interpolation_lut,
-    parse_lookup_table,
 )
 
 from mx_bluesky.common.utils.log import LOGGER
@@ -114,12 +115,15 @@ def adjust_dcm_pitch_roll_vfm_from_lut(
     d_spacing_a: float = yield from bps.rd(
         undulator_dcm.dcm_ref().crystal_metadata_d_spacing_a
     )
+    config_server = ConfigServer(url="https://daq-config.diamond.ac.uk")
+    pitch_energy_table = config_server.get_file_contents(
+        undulator_dcm.pitch_energy_table_path, GenericLookupTable
+    )
+
     bragg_deg = energy_to_bragg_angle(energy_kev, d_spacing_a)
     LOGGER.info(f"Target Bragg angle = {bragg_deg} degrees")
     dcm_pitch_adjuster = lookup_table_adjuster(
-        linear_interpolation_lut(
-            *parse_lookup_table(undulator_dcm.pitch_energy_table_path)
-        ),
+        linear_interpolation_lut(*pitch_energy_table.columns()),
         dcm.xtal_1.pitch_in_mrad,
         bragg_deg,
     )
@@ -128,10 +132,11 @@ def adjust_dcm_pitch_roll_vfm_from_lut(
     LOGGER.info("Waiting for DCM pitch adjust to complete...")
 
     # DCM Roll
+    roll_energy_table = config_server.get_file_contents(
+        undulator_dcm.roll_energy_table_path, GenericLookupTable
+    )
     dcm_roll_adjuster = lookup_table_adjuster(
-        linear_interpolation_lut(
-            *parse_lookup_table(undulator_dcm.roll_energy_table_path)
-        ),
+        linear_interpolation_lut(*roll_energy_table.columns()),
         dcm.xtal_1.roll_in_mrad,
         bragg_deg,
     )
