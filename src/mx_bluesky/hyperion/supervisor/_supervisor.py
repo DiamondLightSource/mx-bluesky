ce52688845b4e394bbe7788e1bb5b80d959fb2a7
Robert Tuck <robert.tuck@diamond.ac.uk>
2025-12-18 15:25:34 +0000

    Additional system tests for supervisor

--- src/mx_bluesky/hyperion/supervisor/_supervisor.py
+++ src/mx_bluesky/hyperion/supervisor/_supervisor.py
@@ -2,6 +2,7 @@ from collections.abc import Sequence
 from pathlib import Path
 
 from blueapi.client.client import BlueapiClient
+from blueapi.client.event_bus import BlueskyStreamingError
 from blueapi.config import ApplicationConfig, ConfigLoader
 from blueapi.core import BlueskyContext
 from blueapi.service.model import TaskRequest
@@ -13,7 +14,7 @@ from mx_bluesky.common.parameters.constants import Status
 from mx_bluesky.common.utils.log import LOGGER
 from mx_bluesky.hyperion.parameters.components import UDCCleanup, UDCDefaultState, Wait
 from mx_bluesky.hyperion.parameters.load_centre_collect import LoadCentreCollect
-from mx_bluesky.hyperion.plan_runner import PlanRunner
+from mx_bluesky.hyperion.plan_runner import PlanError, PlanRunner
 
 
 def create_context() -> BlueskyContext:
@@ -36,49 +37,72 @@ class SupervisorRunner(PlanRunner):
     ):
         super().__init__(bluesky_context, dev_mode)
         self.blueapi_client = BlueapiClient.from_config(client_config)
+        self._current_status = Status.IDLE
 
     def decode_and_execute(
         self, current_visit: str | None, parameter_list: Sequence[MxBlueskyParameters]
     ) -> MsgGenerator:
+        try:
+            yield from self.check_external_callbacks_are_alive()
+        except Exception as e:
+            raise PlanError(f"Exception raised during plan execution: {e}") from e
         # TODO determine what is the instrument session for udc_default_state etc.
         instrument_session = current_visit or "NO_VISIT"
-        for parameters in parameter_list:
-            LOGGER.info(
-                f"Executing plan with parameters: {parameters.model_dump_json(indent=2)}"
-            )
-            match parameters:
-                case LoadCentreCollect():  # TODO
-                    pass
-                case Wait():
-                    yield from bps.sleep(parameters.duration_s)
-                case UDCDefaultState():
-                    task_request = TaskRequest(
-                        name="move_to_udc_default_state",
-                        params={},
-                        instrument_session=instrument_session,
-                    )
-                    self.blueapi_client.run_task(task_request)
-                    yield from bps.null()
-                case UDCCleanup():
-                    task_request = TaskRequest(
-                        name="clean_up_udc",
-                        params={"visit": current_visit},
-                        instrument_session=instrument_session,
-                    )
-                    self.blueapi_client.run_task(task_request)
-                    yield from bps.null()
-                case _:
-                    raise AssertionError(
-                        f"Unsupported instruction decoded from agamemnon {type(parameters)}"
-                    )
+        try:
+            if self._current_status == Status.ABORTING:
+                raise PlanError("Plan execution cancelled, supervisor is shutting down")
+            self._current_status = Status.BUSY
+            for parameters in parameter_list:
+                LOGGER.info(
+                    f"Executing plan with parameters: {parameters.model_dump_json(indent=2)}"
+                )
+                match parameters:
+                    case LoadCentreCollect():  # TODO
+                        pass
+                    case Wait():
+                        yield from bps.sleep(parameters.duration_s)
+                    case UDCDefaultState():
+                        task_request = TaskRequest(
+                            name="move_to_udc_default_state",
+                            params={},
+                            instrument_session=instrument_session,
+                        )
+                        self.blueapi_client.run_task(task_request)
+                        yield from bps.null()
+                    case UDCCleanup():
+                        task_request = TaskRequest(
+                            name="clean_up_udc",
+                            params={"visit": current_visit},
+                            instrument_session=instrument_session,
+                        )
+                        try:
+                            self.blueapi_client.run_task(task_request)
+                        except BlueskyStreamingError as e:
+                            # We will receive a BlueskyStreamingError if the remote server
+                            # processed an abort during plan execution, but this is not
+                            # the only possible cause.
+                            if self.current_status == Status.ABORTING:
+                                LOGGER.info("Aborting local runner...")
+                                self.request_run_engine_abort()
+                            else:
+                                raise PlanError(
+                                    f"Exception raised during plan execution: {e}"
+                                ) from e
+                        yield from bps.null()
+                    case _:
+                        raise AssertionError(
+                            f"Unsupported instruction decoded from agamemnon {type(parameters)}"
+                        )
+        except:
+            self._current_status = Status.FAILED
+            raise
+        else:
+            self._current_status = Status.IDLE
         return current_visit
-
-    def reset_callback_watchdog_timer(self):
-        pass
 
     @property
     def current_status(self) -> Status:
-        return Status.FAILED  # TODO
+        return self._current_status
 
     def is_connected(self) -> bool:
         try:
@@ -89,4 +113,11 @@ class SupervisorRunner(PlanRunner):
         return True
 
     def shutdown(self):
-        pass
+        LOGGER.info(
+            "Hyperion supervisor received shutdown request, signalling abort to BlueAPI server..."
+        )
+        if self.current_status != Status.BUSY:
+            self.request_run_engine_abort()
+        else:
+            self._current_status = Status.ABORTING
+            self.blueapi_client.abort()
