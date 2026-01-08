2f262168313f72958b0b0e314699c16fb6c3a1cc
Robert Tuck <robert.tuck@diamond.ac.uk>
2026-01-08 11:05:30 +0000

    Additional unit tests

--- tests/unit_tests/hyperion/external_interaction/stomp/test_dispatcher.py
+++ tests/unit_tests/hyperion/external_interaction/stomp/test_dispatcher.py
@@ -1,4 +1,49 @@
-def test_dispatcher_connect_and_disconnects_on_enter_and_exit(): ...
+from unittest.mock import MagicMock, Mock
+
+import pytest
+from blueapi.core import DataEvent
+from bluesky_stomp.messaging import MessageContext, StompClient
+
+from mx_bluesky.hyperion.external_interaction.callbacks.stomp.dispatcher import (
+    StompDispatcher,
+)
+
+TEST_TASK_ID = "TEST_TASK_ID"
+
+TEST_SUB_ID = "SUB_ID"
 
 
-def test_dispatcher_forwards_data_event(): ...
+@pytest.fixture
+def mock_client():
+    stomp_client = Mock(spec=StompClient)
+    stomp_client.subscribe.return_value = TEST_SUB_ID
+    return stomp_client
+
+
+def test_dispatcher_connect_and_disconnects_on_enter_and_exit(mock_client):
+    dispatcher = StompDispatcher(mock_client)
+    with dispatcher:
+        assert mock_client.connect.called_once()
+        assert mock_client.subscribe.called_once()
+    assert mock_client.unsubscribe.called_once_with(TEST_SUB_ID)
+    assert mock_client.disconnect.called_once()
+
+
+def test_dispatcher_forwards_data_event(mock_client, test_event_data):
+    dispatcher = StompDispatcher(mock_client)
+    callback = MagicMock()
+
+    with dispatcher:
+        stomp_subs_callback = mock_client.subscribe.mock_calls[0].args[1]
+        dispatcher.subscribe(callback)
+        stomp_subs_callback(
+            DataEvent(
+                name="start",
+                doc=test_event_data.test_grid_detect_and_gridscan_start_document,
+                task_id=TEST_TASK_ID,
+            ),
+            Mock(spec=MessageContext),
+        )
+        callback.assert_called_once_with(
+            "start", test_event_data.test_grid_detect_and_gridscan_start_document
+        )
