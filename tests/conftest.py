75ea25073a7fe7df9f4fb4f2d659d8e1391c7da3
olliesilvester <122091460+olliesilvester@users.noreply.github.com>
2026-01-19 12:06:27 +0000

    Add Jungfrau rotation scan plan (#1234)
    * Add Jungfrau rotation scan plan
    
    * Make compatible with numtracker
    
    * Get metadata writer and experiment params working with numtracker

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,7 +30,14 @@ from dodal.devices.aperturescatterguard import (
     ApertureScatterguard,
     ApertureValue,
 )
-from dodal.devices.attenuator.attenuator import BinaryFilterAttenuator
+from dodal.devices.attenuator.attenuator import (
+    BinaryFilterAttenuator,
+    EnumFilterAttenuator,
+)
+from dodal.devices.attenuator.filter_selections import (
+    I24FilterOneSelections,
+    I24FilterTwoSelections,
+)
 from dodal.devices.backlight import Backlight
 from dodal.devices.baton import Baton
 from dodal.devices.beamsize.beamsize import BeamsizeBase
@@ -92,6 +99,7 @@ from mx_bluesky.common.parameters.constants import (
     PlanNameConstants,
 )
 from mx_bluesky.common.parameters.gridscan import SpecifiedThreeDGridScan
+from mx_bluesky.common.parameters.rotation import RotationScan
 from mx_bluesky.common.utils.exceptions import CrystalNotFoundError
 from mx_bluesky.common.utils.log import (
     ALL_LOGGERS,
@@ -112,7 +120,6 @@ from mx_bluesky.hyperion.parameters.device_composites import (
     HyperionFlyScanXRayCentreComposite,
 )
 from mx_bluesky.hyperion.parameters.gridscan import HyperionSpecifiedThreeDGridScan
-from mx_bluesky.hyperion.parameters.rotation import RotationScan
 
 pytest_plugins = ["tests.expeye_helpers"]
 
@@ -863,6 +870,21 @@ def zocalo():
     zoc.stage = MagicMock(side_effect=lambda: completed_status())
     zoc.unstage = MagicMock(side_effect=lambda: completed_status())
     return zoc
+
+
+@pytest.fixture
+async def enum_attenuator() -> EnumFilterAttenuator:
+    with init_devices(mock=True):
+        attenuator = EnumFilterAttenuator(
+            "", filter_selection=(I24FilterOneSelections, I24FilterTwoSelections)
+        )
+
+    @AsyncStatus.wrap
+    async def fake_attenuator_set(val):
+        set_mock_value(attenuator.actual_transmission, val)
+
+    attenuator.set = MagicMock(side_effect=fake_attenuator_set)
+    return attenuator
 
 
 @pytest.fixture
