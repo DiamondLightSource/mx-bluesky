2db654ec185f6a8a222b6a7e7e09d60a5bd7ffbe
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-01-20 14:09:25 +0000

    Refactor i24 folder structure

---
+++ src/mx_bluesky/beamlines/i24/__init__.py
@@ -0,0 +1,55 @@
+from .serial.extruder.i24ssx_extruder_collect_py3v2 import (
+    enter_hutch,
+    initialise_extruder,
+    laser_check,
+)
+from .serial.fixed_target.i24ssx_chip_manager_py3v1 import (
+    block_check,
+    cs_maker,
+    fiducial,
+    initialise_stages,
+    moveto,
+    moveto_preset,
+)
+from .web_gui_plans.general_plans import (
+    gui_gonio_move_on_click,
+    gui_move_backlight,
+    gui_move_detector,
+    gui_run_chip_collection,
+    gui_run_extruder_collection,
+    gui_set_fiducial_0,
+    gui_set_zoom_level,
+    gui_stage_move_on_click,
+)
+from .web_gui_plans.oav_plans import (
+    focus_on_oav_view,
+    move_block_on_arrow_click,
+    move_nudge_on_arrow_click,
+    move_window_on_arrow_click,
+)
+
+# NOTE. Only plans that will be used by cluster blueapi/web UI
+
+__all__ = [
+    "gui_stage_move_on_click",
+    "gui_gonio_move_on_click",
+    "gui_move_detector",
+    "gui_run_chip_collection",
+    "gui_move_backlight",
+    "gui_set_zoom_level",
+    "gui_set_fiducial_0",
+    "gui_run_extruder_collection",
+    "initialise_extruder",
+    "enter_hutch",
+    "laser_check",
+    "moveto",
+    "moveto_preset",
+    "block_check",
+    "cs_maker",
+    "fiducial",
+    "initialise_stages",
+    "focus_on_oav_view",
+    "move_block_on_arrow_click",
+    "move_nudge_on_arrow_click",
+    "move_window_on_arrow_click",
+]
