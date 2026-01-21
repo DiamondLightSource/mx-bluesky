a6ddc9b42162edc3f2580b14988ecd41a40389a5
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-01-21 16:53:22 +0000

    Rename params

--- src/mx_bluesky/beamlines/i24/web_gui_plans/jungfrau_plans.py
+++ src/mx_bluesky/beamlines/i24/web_gui_plans/jungfrau_plans.py
@@ -13,21 +13,21 @@ from mx_bluesky.beamlines.i24.jungfrau_commissioning.experiment_plans.rotation_s
 @bpp.run_decorator()
 def gui_run_jf_rotation_scan(
     filename: str,
-    exp_time: float,
-    omega_start: float,
-    omega_increment: float,
-    det_distance: float,
+    exposure_time_s: float,
+    omega_start_deg: float,
+    omega_increment_deg: float,
+    det_distance_mm: float,
     sample_id: int,
     transmissions: list[float],
     composite: RotationScanComposite = inject(),
 ):
     params = ExternalRotationScanParams(
         transmission_fractions=transmissions,
-        exposure_time_s=exp_time,
-        omega_start_deg=omega_start,
-        rotation_increment_per_image_deg=omega_increment,
+        exposure_time_s=exposure_time_s,
+        omega_start_deg=omega_start_deg,
+        rotation_increment_per_image_deg=omega_increment_deg,
         filename=filename,
-        detector_distance_mm=det_distance,
+        detector_distance_mm=det_distance_mm,
         sample_id=sample_id,
     )
 
