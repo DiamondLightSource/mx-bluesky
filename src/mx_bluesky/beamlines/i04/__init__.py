9d26d1f4697f180a214af15dd9547fdc56c327c1
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2026-01-30 12:12:04 +0000

    Expose plans

--- src/mx_bluesky/beamlines/i04/__init__.py
+++ src/mx_bluesky/beamlines/i04/__init__.py
@@ -1,8 +1,11 @@
 from mx_bluesky.beamlines.i04.experiment_plans.i04_grid_detect_then_xray_centre_plan import (
     i04_default_grid_detect_and_xray_centre,
 )
+from mx_bluesky.beamlines.i04.expose_bluesky_plan_stubs import move_device, read_device
 from mx_bluesky.beamlines.i04.oav_centering_plans.oav_imaging import (
+    find_beam_centre_at_current_zoom_and_transmission,
     find_beam_centres,
+    optimise_transmission_for_current_zoom,
     optimise_transmission_with_oav,
     take_oav_image_with_scintillator_in,
 )
@@ -20,4 +23,8 @@ __all__ = [
     "take_oav_image_with_scintillator_in",
     "optimise_transmission_with_oav",
     "find_beam_centres",
+    "find_beam_centre_at_current_zoom_and_transmission",
+    "optimise_transmission_for_current_zoom",
+    "move_device",
+    "read_device",
 ]
