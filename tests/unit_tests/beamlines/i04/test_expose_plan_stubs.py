3d332539b61850d31a232bca9ce0ef1d948b4bec
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2026-02-02 16:02:09 +0000

    fix_exposed_plan_stubs

--- tests/unit_tests/beamlines/i04/test_expose_plan_stubs.py
+++ tests/unit_tests/beamlines/i04/test_expose_plan_stubs.py
@@ -1,34 +1,51 @@
+from unittest.mock import AsyncMock
+
+import pytest
+from bluesky import FailedStatus
+from bluesky.run_engine import RunEngine
 from bluesky.simulators import RunEngineSimulator, assert_message_and_return_remaining
-from dodal.devices.scintillator import InOut, Scintillator
+from dodal.devices.scintillator import Scintillator
+from ophyd_async.core import (
+    get_mock_put,
+)
 
-from mx_bluesky.beamlines.i04.expose_plan_stubs import do_plan_stup
+from mx_bluesky.beamlines.i04.expose_plan_stubs import move, read
 
 
-def test_do_plan_stub_on_scintillator_move_in(
+def test_do_move_in_on_scintillator(
+    scintillator: Scintillator,
+    run_engine: RunEngine,
+):
+    scintillator._check_aperture_parked = AsyncMock()
+    run_engine(move(scintillator.selected_pos, "In"))
+    get_mock_put(scintillator.selected_pos).assert_called_once_with("In", wait=True)
+
+
+def test_do_move_out_on_scintillator(
+    scintillator: Scintillator,
+    run_engine: RunEngine,
+):
+    scintillator._check_aperture_parked = AsyncMock()
+    run_engine(move(scintillator.selected_pos, "Out"))
+    get_mock_put(scintillator.selected_pos).assert_called_once_with("Out", wait=True)
+
+
+def test_do_move_with_invalid_location_causes_error(
+    scintillator: Scintillator,
+    run_engine: RunEngine,
+):
+    scintillator._check_aperture_parked = AsyncMock()
+    with pytest.raises(FailedStatus):
+        run_engine(move(scintillator.selected_pos, "invalid"))
+
+
+def test_read_does_a_read(
     scintillator: Scintillator,
     sim_run_engine: RunEngineSimulator,
 ):
-    msgs = sim_run_engine.simulate_plan(
-        do_plan_stup("mv", "scintillator", "In", scintillator=scintillator)
-    )
-    msgs = assert_message_and_return_remaining(
+    scintillator._check_aperture_parked = AsyncMock()
+    msgs = sim_run_engine.simulate_plan(read(scintillator))
+    assert_message_and_return_remaining(
         msgs,
-        lambda msg: msg.command == "set"
-        and msg.obj.name == "scintillator"
-        and msg.args[0] == InOut.IN,
-    )
-
-
-def test_do_plan_stub_on_scintillator_move_out(
-    scintillator: Scintillator,
-    sim_run_engine: RunEngineSimulator,
-):
-    msgs = sim_run_engine.simulate_plan(
-        do_plan_stup("mv", "scintillator", "Out", scintillator=scintillator)
-    )
-    msgs = assert_message_and_return_remaining(
-        msgs,
-        lambda msg: msg.command == "set"
-        and msg.obj.name == "scintillator"
-        and msg.args[0] == InOut.OUT,
+        (lambda msg: msg.command == "read" and msg.obj.name == "scintillator"),
     )
