4850240541f0829efb5d844a9e3fff61df08fe2e
Robert Tuck <robert.tuck@diamond.ac.uk>
2026-02-24 10:57:55 +0000

    Remove obsolete tests for wrapped smargon

--- tests/unit_tests/common/experiment_plans/test_grid_detection_plan.py
+++ tests/unit_tests/common/experiment_plans/test_grid_detection_plan.py
@@ -1,4 +1,3 @@
-from collections.abc import Generator
 from pathlib import Path
 from typing import Any, Literal
 from unittest.mock import DEFAULT, AsyncMock, MagicMock, patch
@@ -9,8 +8,7 @@ import pytest
 from bluesky.run_engine import RunEngine
 from bluesky.simulators import RunEngineSimulator, assert_message_and_return_remaining
 from bluesky.utils import Msg
-from dodal.beamlines import i03, i04
-from dodal.common.beamlines.beamline_utils import clear_devices
+from dodal.beamlines import i03
 from dodal.devices.backlight import Backlight
 from dodal.devices.oav.oav_detector import OAVConfigBeamCentre
 from dodal.devices.oav.oav_parameters import OAVParameters
@@ -530,68 +528,30 @@ def test_given_array_with_all_invalid_top_and_bottom_sections_then_min_and_max_i
     assert max_y == expected_max
 
 
-@pytest.fixture
-def unwrapped_smargon() -> Generator[Smargon, None, None]:
-    smargon = i04.smargon.build(connect_immediately=True, mock=True)
-    # Initial positions, needed for stub_offsets
-    set_mock_value(smargon.stub_offsets.center_at_current_position.disp, 0)
-    yield smargon
-    clear_devices()
-
-
-@pytest.fixture
-def wrapped_smargon(smargon) -> Generator[Smargon, None, None]:
-    yield smargon
-
-
-@pytest.fixture
-def smargons(request, smargon, unwrapped_smargon):
-    return smargon if request.param == "wrapped_smargon" else unwrapped_smargon
-
-
 @pytest.mark.parametrize(
-    "smargons, omega, expected_sequence",
+    "omega, expected_sequence",
     [
-        ["wrapped_smargon", 0, [0, -90]],
-        ["wrapped_smargon", 5, [0, -90]],
-        ["wrapped_smargon", -5, [0, -90]],
-        ["wrapped_smargon", -44, [0, -90]],
-        ["wrapped_smargon", -46, [-90, -0]],
-        ["wrapped_smargon", -90, [-90, -0]],
-        ["wrapped_smargon", -135, [-90, -0]],
-        ["wrapped_smargon", -224, [-90, -0]],
-        ["wrapped_smargon", -225, [0, -90]],
-        ["wrapped_smargon", -226, [0, -90]],
-        ["wrapped_smargon", 90, [0, -90]],
-        ["wrapped_smargon", 134, [0, -90]],
-        ["wrapped_smargon", 135, [0, -90]],
-        ["wrapped_smargon", 136, [-90, 0]],
-        ["wrapped_smargon", 180, [-90, -0]],
-        ["wrapped_smargon", 270, [-90, 0]],
-        ["wrapped_smargon", 290, [-90, 0]],
-        ["wrapped_smargon", 330, [0, -90]],
-        # ["unwrapped_smargon", 0, [0, -90]],
-        # ["unwrapped_smargon", 5, [0, -90]],
-        # ["unwrapped_smargon", -5, [0, -90]],
-        # ["unwrapped_smargon", -44, [0, -90]],
-        # ["unwrapped_smargon", -46, [-90, -0]],
-        # ["unwrapped_smargon", -90, [-90, -0]],
-        # ["unwrapped_smargon", -135, [-90, -0]],
-        # ["unwrapped_smargon", -224, [-90, 0]],
-        # ["unwrapped_smargon", -225, [-90, 0]],
-        # ["unwrapped_smargon", -226, [-90, 0]],
-        # ["unwrapped_smargon", 90, [0, -90]],
-        # ["unwrapped_smargon", 134, [0, -90]],
-        # ["unwrapped_smargon", 135, [0, -90]],
-        # ["unwrapped_smargon", 136, [0, -90]],
-        # ["unwrapped_smargon", 180, [0, -90]],
-        # ["unwrapped_smargon", 270, [0, -90]],
-        # ["unwrapped_smargon", 290, [0, -90]],
-        # ["unwrapped_smargon", 330, [0, -90]],
+        [0, [0, -90]],
+        [5, [0, -90]],
+        [-5, [0, -90]],
+        [-44, [0, -90]],
+        [-46, [-90, -0]],
+        [-90, [-90, -0]],
+        [-135, [-90, -0]],
+        [-224, [-90, -0]],
+        [-225, [0, -90]],
+        [-226, [0, -90]],
+        [90, [0, -90]],
+        [134, [0, -90]],
+        [135, [0, -90]],
+        [136, [-90, 0]],
+        [180, [-90, -0]],
+        [270, [-90, 0]],
+        [290, [-90, 0]],
+        [330, [0, -90]],
     ],
-    indirect=["smargons"],
 )
-def test_optimum_grid_detect_angles(smargons, run_engine, omega, expected_sequence):
-    set_mock_value(smargons.omega.user_readback, omega)
-    result = run_engine(optimum_grid_detect_angles(smargons))
+def test_optimum_grid_detect_angles(smargon, run_engine, omega, expected_sequence):
+    set_mock_value(smargon.omega.user_readback, omega)
+    result = run_engine(optimum_grid_detect_angles(smargon))
     assert result.plan_result == expected_sequence
