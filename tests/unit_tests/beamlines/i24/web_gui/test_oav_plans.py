20d480a66b9d02ddcb8dbbd5a2dc8752ac25b6cd
Tamoor Shahid <tamoor_shahid@hotmail.com>
2026-03-05 16:28:52 +0000

    Added test

--- tests/unit_tests/beamlines/i24/web_gui/test_oav_plans.py
+++ tests/unit_tests/beamlines/i24/web_gui/test_oav_plans.py
@@ -9,6 +9,7 @@ from mx_bluesky.beamlines.i24.web_gui_plans.oav_plans import (
     focus_on_oav_view,
     move_block_on_arrow_click,
     move_nudge_on_arrow_click,
+    move_on_oav_view_click,
     move_window_on_arrow_click,
 )
 
@@ -103,3 +104,20 @@ async def test_focus_on_oav_view(
 ):
     run_engine(focus_on_oav_view(FocusDirection(direction), MoveSize(move_size), pmac))
     assert await pmac.z.user_readback.get_value() == expected_value
+
+
+@pytest.mark.parametrize(
+    "coordinates, expected_x, expected_y",
+    [
+        ((568, 321), 0.00586, 0.00321),
+        ((123, 789), 0.00123, 0.00789),
+    ],
+)
+def test_move_on_oav_view_click(
+    coordinates, expected_x, expected_y, oav, pmac, run_engine
+):
+    with patch(
+        "mx_bluesky.beamlines.i24.web_gui_plans.oav_plans.bps.mv",
+    ) as mock_bps_mv:
+        run_engine(move_on_oav_view_click(coordinates, oav, pmac))
+        mock_bps_mv.assert_any_call(pmac.x, expected_x, pmac.y, expected_y, wait=True)
