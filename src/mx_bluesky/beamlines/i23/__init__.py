cc1081daa781940f7dfe15879e6759f81f00def5
Tamoor Shahid <tamoor_shahid@hotmail.com>
2026-04-23 14:50:57 +0100

    Added plan to i23 init

--- src/mx_bluesky/beamlines/i23/__init__.py
+++ src/mx_bluesky/beamlines/i23/__init__.py
@@ -1,3 +1,6 @@
 from mx_bluesky.beamlines.i23.serial import serial_collection
+from mx_bluesky.common.experiment_plans.pin_tip_centring_plan import (
+    pin_tip_centre_plan,
+)
 
-__all__ = ["serial_collection"]
+__all__ = ["serial_collection", "pin_tip_centre_plan"]
