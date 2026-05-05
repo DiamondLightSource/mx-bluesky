0a2737f4ec2bafb2e19a581cd0890b3e64f95daa
Tamoor Shahid <tamoor_shahid@hotmail.com>
2026-05-05 14:12:09 +0100

    Updated Hello.py and centering.py

--- src/mx_bluesky/beamlines/i23/hello.py
+++ src/mx_bluesky/beamlines/i23/hello.py
@@ -1,3 +1,12 @@
+import bluesky.plan_stubs as bps
+
+from mx_bluesky.common.utils.log import LOGGER
+
+
 def hello():
-    """Plan to print 'Hello World' to the console to test plan works."""
-    print("Hello World")
+    """Plan to log 'Hello World' to the GDA log panel via Bluesky logging."""
+    LOGGER.info("Hello World from I23 Bluesky plan!")
+
+    # Example: Read a value and log it
+    yield from bps.sleep(0.1)  # Yield to Bluesky to keep it a valid plan
+    LOGGER.info("Plan completed successfully")
