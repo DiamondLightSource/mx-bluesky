93b2fde76fd98951994e895e66a83762a9a2183d
olliesilvester <122091460+olliesilvester@users.noreply.github.com>
2026-05-13 13:55:44 +0100

    Add vmxm gridscan plan (#1636)
    * Implement Vmxm gridscan plan
    * Make common_flyscan_xray_centre plan accept SpecifiedGrids instead of SpecifiedThreeDGridScan
    
    ---------
    
    Co-authored-by: Robert Tuck <robert.tuck@diamond.ac.uk>

--- tests/unit_tests/common/parameters/test_gridscan.py
+++ tests/unit_tests/common/parameters/test_gridscan.py
@@ -39,25 +39,7 @@ def test_specified_grids_validation_error(
     y_steps: list[int],
     should_raise: bool,
 ):
-    if should_raise:
-        with pytest.raises(
-            ValidationError, match="Fields must all have the same length:"
-        ):
-            GridParamsTest(
-                x_start_um=0,
-                y_starts_um=y_starts_um,
-                z_starts_um=z_starts_um,
-                omega_starts_deg=omega_starts_deg,
-                y_step_sizes_um=y_step_sizes_um,
-                y_steps=y_steps,
-                sample_id=0,
-                visit="/tmp",
-                parameter_model_version=get_param_version(),
-                file_name="/tmp",
-                storage_directory="/tmp",
-                x_steps=5,
-            )
-    else:
+    def make_params():
         GridParamsTest(
             x_start_um=0,
             y_starts_um=y_starts_um,
@@ -73,11 +55,27 @@ def test_specified_grids_validation_error(
             x_steps=5,
         )
 
+    if should_raise:
+        with pytest.raises(
+            ValidationError, match="Fields must all have the same length:"
+        ):
+            make_params()
+    else:
+        make_params()
+
+
+class SpecifiedThreeDTest(SpecifiedThreeDGridScan):
+    # Skip parent validation for easier testing
+    def _check_lengths_are_same(self):  # type: ignore
+        return self
+
 
 @pytest.mark.parametrize(
     "y_starts_um, z_starts_um, omega_starts_deg, y_step_sizes_um, y_steps, should_raise",
     [
         ([1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1], True),
+        ([1, 1], [1, 1], [1, 1], [1, 1, 1], [1, 1], True),
+        ([1, 1], [1, 1], [1, 1, 1], [1, 1], [1, 1], True),
         ([1, 1], [1, 1], [1, 1], [1, 1], [1, 1], False),
     ],
 )
@@ -89,19 +87,24 @@ def test_three_d_grid_scan_validation(
     y_steps: list[int],
     should_raise: bool,
 ):
+    def make_params():
+        SpecifiedThreeDTest(
+            x_start_um=0,
+            y_starts_um=y_starts_um,
+            z_starts_um=z_starts_um,
+            omega_starts_deg=omega_starts_deg,
+            y_step_sizes_um=y_step_sizes_um,
+            y_steps=y_steps,
+            sample_id=0,
+            visit="/tmp",
+            parameter_model_version=get_param_version(),
+            file_name="/tmp",
+            storage_directory="/tmp",
+            x_steps=5,
+        )
+
     if should_raise:
         with pytest.raises(ValidationError, match="must be length 2 for 3D scans"):
-            SpecifiedThreeDGridScan(
-                x_start_um=0,
-                y_starts_um=y_starts_um,
-                z_starts_um=z_starts_um,
-                omega_starts_deg=omega_starts_deg,
-                y_step_sizes_um=y_step_sizes_um,
-                y_steps=y_steps,
-                sample_id=0,
-                visit="/tmp",
-                parameter_model_version=get_param_version(),
-                file_name="/tmp",
-                storage_directory="/tmp",
-                x_steps=5,
-            )
+            make_params()
+    else:
+        make_params()
