f801a93dd888f3e36c5c3a2ebd741267e3659fe9
Robert Tuck <robert.tuck@diamond.ac.uk>
2026-06-22 12:39:09 +0100

    Mark git repos as safe directory

--- utility_scripts/docker/initialise_scratch.sh
+++ utility_scripts/docker/initialise_scratch.sh
@@ -5,6 +5,9 @@
 SCRATCH_ROOT=/scratch
 APP_ROOT=/app
 
+git config --global -add safe.directory /app/dodal/.git
+git config --global -add safe.directory /app/mx-bluesky/.git
+
 if [ ! -d $SCRATCH_ROOT/dodal/.git ]; then
   git clone /app/dodal/.git $SCRATCH_ROOT/dodal
   echo "Checking out dodal branch $DODAL_BRANCH"
