e0a4828648c85561981d317e0996bb5fd6035b9a
Robert Tuck <robert.tuck@diamond.ac.uk>
2026-06-22 11:14:20 +0100

    Update build_docker_image to push the helm chart

--- utility_scripts/build_docker_image.sh
+++ utility_scripts/build_docker_image.sh
@@ -4,6 +4,7 @@ set -eo pipefail
 BUILD=1
 PUSH=0
 BUILD_UNCLEAN=0
+BUILD_HELM=0
 PODMAN_FLAGS=""
 for option in "$@"; do
     case $option in
@@ -23,15 +24,20 @@ for option in "$@"; do
             BUILD_UNCLEAN=1
             shift
             ;;
+        --helm)
+          BUILD_HELM=1
+          shift
+          ;;
         --help|--info|--h)
             CMD=`basename $0`
             echo "$CMD [options]"
-            echo "Builds a development docker image and optionally pushes the docker container image to the repository"
+            echo "Builds a development hyperion docker image and optionally pushes the docker container image to the repository"
             echo "  --help                  This help"
             echo "  --no-build              Do not build the image"
             echo "  --push                  Push the image"
             echo "  --no-cache              Don't use the cache when building the image."
             echo "  --unclean               Build with an unclean workspace"
+            echo "  --helm                  Build (and optionally push) the helm chart"
             exit 0
             ;;
         -*|--*)
@@ -57,27 +63,39 @@ if [[ $BUILD_UNCLEAN == 0 ]]; then
   fi
 fi
 
+export BIGFILES_TMPDIR=/scratch/tmp
+export TMPDIR=/tmp
+
 if [[ $BUILD == 1 ]]; then
   echo "Building initial image"
   IMAGE_VERSION=$(extract_version)
   MX_BLUESKY_VERSION=${IMAGE_VERSION/-/+}
   LATEST_TAG=$IMAGE:dev
-  BIGFILES_TMPDIR=/scratch/tmp TMPDIR=/tmp podman build \
-    $PODMAN_FLAGS \
-    --build-arg SETUPTOOLS_SCM_PRETEND_VERSION_FOR_MX_BLUESKY=$MX_BLUESKY_VERSION \
-    -f $PROJECTDIR/Dockerfile.hyperion \
-    --tag $LATEST_TAG \
-    --tag $IMAGE:$IMAGE_VERSION \
-    $PROJECTDIR
+  if [[ $BUILD_HELM == 0 ]]; then
+    podman build \
+      $PODMAN_FLAGS \
+      --build-arg SETUPTOOLS_SCM_PRETEND_VERSION_FOR_MX_BLUESKY=$MX_BLUESKY_VERSION \
+      -f $PROJECTDIR/Dockerfile.hyperion \
+      --tag $LATEST_TAG \
+      --tag $IMAGE:$IMAGE_VERSION \
+      $PROJECTDIR
+    else
+      helm dependencies update helm/$IMAGE
+      helm package helm/$IMAGE --version ${IMAGE_VERSION} --app-version ${IMAGE_VERSION} -d /tmp/
+    fi
 fi
 
 if [[ $PUSH == 1 ]]; then
   NAMESPACE=$(podman login --get-login ghcr.io | tr '[:upper:]' '[:lower:]')
-  if [[ $? != 0 ]]; then
-    echo "Not logged in to ghcr.io"
-    exit 1
+  if [[ $BUILD_HELM == 0 ]]; then
+    if [[ $? != 0 ]]; then
+      echo "Not logged in to ghcr.io"
+      exit 1
+    fi
+    echo "Pushing to ghcr.io/$NAMESPACE/$IMAGE:dev ..."
+    podman push $IMAGE:dev docker://ghcr.io/$NAMESPACE/$IMAGE:dev
+    podman push $IMAGE:dev docker://ghcr.io/$NAMESPACE/$IMAGE:$IMAGE_VERSION
+  else
+    helm push /tmp/hyperion-${IMAGE_VERSION}.tgz oci://ghcr.io/$NAMESPACE/charts
   fi
-  echo "Pushing to ghcr.io/$NAMESPACE/$IMAGE:dev ..."
-  podman push $IMAGE:dev docker://ghcr.io/$NAMESPACE/$IMAGE:dev
-  podman push $IMAGE:dev docker://ghcr.io/$NAMESPACE/$IMAGE:$IMAGE_VERSION
 fi
