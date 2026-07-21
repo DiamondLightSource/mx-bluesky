a727122f8dd301493d64d359f1bf08253d054528
Robert Tuck <robert.tuck@diamond.ac.uk>
2026-07-21 10:52:45 +0100

    Change pin_centre_then_gridscan_plan signatures for generics

--- src/mx_bluesky/hyperion/experiment_plans/pin_centre_then_gridscan_plan.py
+++ src/mx_bluesky/hyperion/experiment_plans/pin_centre_then_gridscan_plan.py
@@ -6,6 +6,7 @@ from dodal.devices.oav.oav_parameters import OAVParameters
 
 from mx_bluesky.common.device_setup_plans.manipulate_sample import move_phi_chi
 from mx_bluesky.common.experiment_plans.common_grid_detect_then_xray_centre_plan import (
+    TGridDetectAndGridScanEssentialDevices,
     detect_grid_and_do_gridscan,
 )
 from mx_bluesky.common.experiment_plans.inner_plans.xrc_results_utils import (
@@ -23,9 +24,6 @@ from mx_bluesky.common.external_interaction.callbacks.grid.grid_detect_and_scan.
     ispyb_activation_wrapper,
 )
 from mx_bluesky.common.parameters.constants import OavConstants, PlanNameConstants
-from mx_bluesky.common.parameters.device_composites import (
-    GridDetectAndGridScanEssentialDevices,
-)
 from mx_bluesky.common.preprocessors.preprocessors import (
     pause_xbpm_feedback_during_collection_at_desired_transmission_decorator,
 )
@@ -39,7 +37,7 @@ from mx_bluesky.hyperion.parameters.gridscan import (
 
 
 def pin_centre_then_gridscan_plan(
-    composite: GridDetectAndGridScanEssentialDevices,
+    composite: TGridDetectAndGridScanEssentialDevices,
     parameters: PinTipCentreThenXrayCentre,
     detector_params: DetectorParams,
     oav_config_file: str = OavConstants.OAV_CONFIG_JSON,
