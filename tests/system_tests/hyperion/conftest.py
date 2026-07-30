2c78d1136d25d3b56c1939871f751488c03a35fd
Robert Tuck <robert.tuck@diamond.ac.uk>
2026-07-30 11:23:09 +0100

    System tests passing

--- tests/system_tests/hyperion/conftest.py
+++ tests/system_tests/hyperion/conftest.py
@@ -5,5 +5,5 @@ import pytest
 
 @pytest.fixture
 def use_classic_eiger():
-    with patch("mx_bluesky.hyperion.blueapi.composites.use_fast_cs_eiger", True):
+    with patch("mx_bluesky.hyperion.blueapi.composites.use_fast_cs_eiger", False):
         yield
