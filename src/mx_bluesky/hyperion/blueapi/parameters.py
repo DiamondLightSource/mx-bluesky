d8c0212756cbcd3048edc2c5927cba98b10fd8a7
rtuck99 <robert.tuck@diamond.ac.uk>
2026-09-07 09:44:30 +0100

    Set aperture according to XRC results (#1705)
    * Implement AperturePolicy and automatic selection of Aperture based on GridScan results

--- src/mx_bluesky/hyperion/blueapi/parameters.py
+++ src/mx_bluesky/hyperion/blueapi/parameters.py
@@ -2,12 +2,13 @@
 This module contains the parameter models exported via the hyperion-blueapi REST interface.
 """
 
-from typing import Any, Literal, TypeAlias
+from typing import Any, Literal, Self, TypeAlias
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
 from pydantic.config import ConfigDict
 
 from mx_bluesky.common.parameters.components import (
+    AperturePolicy,
     get_param_version,
 )
 from mx_bluesky.common.parameters.constants import GridscanParamConstants
@@ -68,6 +69,7 @@ class MultiRotationScanParams(HyperionParam):
     snapshot_omegas_deg: list[float]
     rotation_scans: list[SingleRotationScanParams]
     transmission_frac: float
+    selected_aperture: AperturePolicy
     ispyb_experiment_type: str
 
 
@@ -90,6 +92,17 @@ class LoadCentreCollectParams(WithCentreSelection, HyperionParam):
     demand_energy_ev: float
     robot_load_then_centre: RobotLoadThenCentreParams
     multi_rotation_scan: MultiRotationScanParams
+
+    @model_validator(mode="after")
+    def _validate_model(self) -> Self:
+        if (
+            self.multi_rotation_scan.selected_aperture
+            == AperturePolicy.CURRENT_POSITION
+        ):
+            raise ValueError(
+                "selected_aperture of CURRENT_POSITION is not supported for LoadCentreCollectParams"
+            )
+        return self
 
 
 def load_centre_collect_to_internal(
